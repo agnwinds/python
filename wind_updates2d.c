d6d58f0d3efcca95289132ad5b9eab468b2a780b
Nicholas Higginbottom <nick_higginbottom@fastmail.fm>
2013-07-29 16:37:56 +0100

    small

--- wind_updates2d.c
+++ wind_updates2d.c
@@ -222,12 +222,15 @@ WindPtr (w);
 /* 71 - 111279 - ksl - Small modification to reflect the fact that nxfreq has been moved into the geo structure */
       for (i = 0; i < geo.nxfreq; i++)	/*loop over number of bands */
 	{
+	Log_parallel("CALC SD cell %i band %i nphot=%i av=%e j=%e sdfreq=%e volume=%e\n",n,i,plasmamain[n].nxtot[i],plasmamain[n].xave_freq[i],plasmamain[n].xj[i],plasmamain[n].xsd_freq[i],volume);
 	  if (plasmamain[n].nxtot[i] > 0)	/*Check we actually have some photons in the cell in this band */
 	    {
+
 	      plasmamain[n].xave_freq[i] /= plasmamain[n].xj[i];	/*Normalise the average frequency */
 	      plasmamain[n].xsd_freq[i] /= plasmamain[n].xj[i];	/*Normalise the mean square frequency */
 	      plasmamain[n].xsd_freq[i] = sqrt (plasmamain[n].xsd_freq[i] - (plasmamain[n].xave_freq[i] * plasmamain[n].xave_freq[i]));	/*Compute standard deviation */
 	      plasmamain[n].xj[i] /= (4 * PI * volume);	/*Convert to radiation density */
+
 	    }
 	  else
 	    {
