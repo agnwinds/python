858d2e5b8828695c70b2a8b9429198bd5f4f3b05
jhmatthews <jimim@hotmail.co.uk>
2014-01-09 17:55:19 +0000

    added test version of He I chianti data

---
+++ atomic_macro/hetest1_elem_ions.py
@@ -0,0 +1,18 @@
+
+
+# Output format: Name z  Relative_abun atomic_weight
+
+
+# Source Verner, Barthel & Tytler 1994 A&AS 108, 287
+Element    2   He   10.99     4.002602
+
+
+# the 20 is the number of "non-LTE" levels to be used - this is infact the
+# number of levels to be treated (for my purposes). 
+
+IonM   He   2   1   1   24.58800   11  11     1s^2(1S_0)
+IonM   He   2   2   2   54.41800   10  10     1s(2S_{1/2})
+IonM   He   2   3   1 1.0000e+20    1   1     Bare
+ 
+
+
