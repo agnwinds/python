d85613b6c08dc6ff74439aab7ae22bbec9c47ee0
jhmatthews <jimim@hotmail.co.uk>
2014-01-20 17:20:13 +0000

    Added mihalas and stone collisional He data

---
+++ atomic_macro/he_coll_coeff.py
@@ -0,0 +1,19 @@
+#	data from Mihalas & Stone 1938
+#	coefficients for approximations 
+#   level configurations should be matched with those in the levels files
+#   first coefficients for forbidden transitions between n=2 states of He
+# 
+# 			z   istate  n    type       c0 			c1 		    c-2 	        (spacers)      iconf  jconf 	ilv		jlv
+CollMacro	2   1       2      2     9.6367493  -2.2294083   -17.301025  0.0 0.0 0.0 0.0 0.0   2S3	  2S1		2       3	
+CollMacro	2   1       2      2     10.855774  -2.4029312   -27.009033  0.0 0.0 0.0 0.0 0.0   2S3    2P1       2       5
+CollMacro	2   1       2      2     8.3804321  -2.0196686   -7.3662109  0.0 0.0 0.0 0.0 0.0   2S1    2P3       3       4
+CollMacro	2   1       2      2     6.958252   -2.0196686   -5.987793   0.0 0.0 0.0 0.0 0.0   2P3    2P1       4       5
+#
+#	coefficients for transitions from the ground state
+#
+#           z   istate    n    type alpha   a0    a1     b1   c1       a2     b2    c2         iconf  jconf    ilv    2S+1_upper
+CollMacro   2     1       1    1    1.125  1.535  2.76   1.2  0.494   0.108  2.50  1.50        1S1    nS1      1      1
+CollMacro   2     1       1    1    1.24   0.230  17.8   1.0  0.080   5.400  1.00  0.35        1S1    nS3      1      3
+CollMacro   2     1       1    1    1.71   0.150  12.3   1.0  0.020   2.600  1.25  0.60        1S1    nP3      1      3
+CollMacro   2     1       1    1    1.02   1.240  1.68   1.2  0.906   0.181  2.70  1.45        1S1    nD1      1      1
+CollMacro   2     1       1    1    0.745  0.455  4.78   1.0  0.500  -0.075  1.50  2.00        1S1    nD3      1      3
