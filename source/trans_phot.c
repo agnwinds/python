847464b72608c57ee955794c1eddef138dcf5a1c
Knox S. Long <long@stsci.edu>
2015-08-27 10:30:05 -0500

    Fix to small problem with the way the check of dfudge was carred out in trans_phot

--- source/trans_phot.c
+++ source/trans_phot.c
@@ -605,7 +605,7 @@ trans_phot_single (WindPtr w, PhotPtr p, int iextract)
          of how many 4 photons were lost due to DFUDGE pushing them 
          outside of the wind after scatter */
 	  // XXX PLACEHOLDER Check that this is the correct logic here 
-	  if (where_in_wind (pp.x,&ndom) != W_ALL_INWIND && where_in_wind (&x_dfudge_check[3],&ndom) == W_ALL_INWIND)
+	  if (where_in_wind (pp.x,&ndom) != W_ALL_INWIND && where_in_wind (x_dfudge_check,&ndom) == W_ALL_INWIND)
 	  {
       	n_lost_to_dfudge++;		// increment the counter (checked at end of trans_phot)
 	  }
