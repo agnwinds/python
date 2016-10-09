a97ce2f14fc770b60b07a49a70642f36e8e9454d
Knox S. Long <long@stsci.edu>
2016-10-09 15:52:59 -0500

    Added a capability to rdpar that allows one to seamlessly read certain old .pf files in which the name for a rdpar keyword has been changed to a new one.
    The map between the old and new keywords are hardwired in synonyms.c

---
+++ source/synonyms.c
@@ -0,0 +1,102 @@
+
+
+
+/**************************************************************************
+                    Space Telescope Science Institute
+
+
+  Synopsis:  
+
+  This is a routine which is intended to help with program updates that
+  change the name of a keyword in a parameter file.  It is called from
+  rdpar.string_process_from_file
+
+  Description:	
+
+  The routine simply matches the string in new_question to a string in
+  the array new_names, below.  If it finds a match, the old_name is 
+  returned in old_question.
+
+
+
+  Arguments:  	
+
+
+  Returns:
+
+  	0 if there was no match
+	1 if there was a match of a name in the parameter file to
+	  one of the new_names, and in old_question the name of
+	  the old_value
+
+  Notes:
+
+  To add another variable to the list, just record the new_name and the
+  old_name in the arrays below, and increase the number of names by 1
+
+  Do not include the material that is in paren, that is for
+
+  xxx(many_choices) -->  xxxx
+
+  The routine is completely hardwired as wrtten though clearly this 
+  could be changed.
+  	
+
+
+  History:
+	16sept	ksl	Coded as part of an effort to make python more 
+			robust to changes in parameter file names
+
+ ************************************************************************/
+
+#include <stdio.h>
+#include <strings.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define	LINELEN 132
+
+
+char *old_names[]={"mstar","rstar"};
+char *new_names[]={"Central.object.mass","Central.object.radius"};
+int nunber_of_names=2;
+
+
+
+int
+check_synonyms (new_question, old_question)
+	char new_question[],old_question[];
+{
+  int n;
+  char *line;
+  char firstword[LINELEN];
+  int nwords, wordlength;
+
+// Strip off any extra bits in the new question
+  line = new_question;
+  strcpy (firstword, "");
+  nwords = sscanf (line, "%s ", firstword);
+  wordlength = strlen (firstword);
+
+  if (nwords == 0)
+    {
+      return (0);
+    }
+
+
+  for (n = 0; n < nunber_of_names; n++)
+    {
+      if (strncmp (new_question, firstword, wordlength) == 0)
+	{
+	  printf ("Matched keyword %s in .pf file to %s in current python version\n",new_question,old_names[n]);
+	  strcpy(old_question,old_names[n]);
+	  return (1);
+	}
+
+    }
+
+  return(0);
+
+}
