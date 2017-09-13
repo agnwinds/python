977597769ce76469b6ad61ca84854e3b7fc05803
jhmatthews <jm8g08@soton.ac.uk>
2017-09-13 14:46:23 +0100

    increased coll strength entries read up to max of 20

--- source/get_atomicdata.c
+++ source/get_atomicdata.c
@@ -2536,8 +2536,11 @@ BAD_T_RR  5  0  1  1  4.647E-10  0.7484  6.142E+01  1.753E+07*/
                 Error ("Get_atomic_data: %s\n", aline);
                 exit (0);
               }
-              nparam = sscanf (aline, "%*s %le %le %le %le %le %le %le %le %le %le",
-                               &temp[0], &temp[1], &temp[2], &temp[3], &temp[4], &temp[5], &temp[6], &temp[7], &temp[8], &temp[9]);
+
+              /* JM 1709 -- increased number of entries read up to max of 20 */
+              nparam = sscanf (aline, "%*s %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le",
+                               &temp[0], &temp[1], &temp[2], &temp[3], &temp[4], &temp[5], &temp[6], &temp[7], &temp[8], &temp[9],
+                               &temp[10], &temp[11], &temp[12], &temp[13], &temp[14], &temp[15], &temp[16], &temp[17], &temp[18], &temp[19]);
 
               for (nn = 0; nn < np; nn++)
               {
@@ -2549,8 +2552,11 @@ BAD_T_RR  5  0  1  1  4.647E-10  0.7484  6.142E+01  1.753E+07*/
                 Error ("Get_atomic_data: %s\n", aline);
                 exit (0);
               }
-              nparam = sscanf (aline, "%*s %le %le %le %le %le %le %le %le %le %le",
-                               &temp[0], &temp[1], &temp[2], &temp[3], &temp[4], &temp[5], &temp[6], &temp[7], &temp[8], &temp[9]);
+
+              /* JM 1709 -- increased number of entries read up to max of 20 */
+              nparam = sscanf (aline, "%*s %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le %le",
+                               &temp[0], &temp[1], &temp[2], &temp[3], &temp[4], &temp[5], &temp[6], &temp[7], &temp[8], &temp[9],
+                               &temp[10], &temp[11], &temp[12], &temp[13], &temp[14], &temp[15], &temp[16], &temp[17], &temp[18], &temp[19]);
 
               for (nn = 0; nn < np; nn++)
               {
