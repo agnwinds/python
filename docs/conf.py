4f8165c342bb443818c82c70aa5764ca7049337c
Sam Mangham <mangham@gmail.com>
2018-01-14 19:02:53 +0000

    Changed theme

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ author = 'Knox Long, Christian Knigge, Stuart Sims, Nick Higginbottom, James Mat
 # built documents.
 #
 # The short X.Y version.
-version = '81a'
+version = '81'
 # The full version, including alpha/beta/rc tags.
 release = '81a'
 
@@ -85,7 +85,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -169,6 +170,3 @@ texinfo_documents = [
      author, 'python', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
