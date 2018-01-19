1af2282e2ff192a356f93ff19634b2cebce79b5a
Sam Mangham <mangham@gmail.com>
2018-01-19 14:12:03 +0000

    Autogenerate with options

--- py_progs/autogenerate_parameter_docs.py
+++ py_progs/autogenerate_parameter_docs.py
@@ -29,7 +29,7 @@ def parse_param_to_dict(found_parameters, text, input_file, param_type):
         param_dict.pop('values', None)
 
     # If this is an 'advanced' parameter, record so
-    if text[0] is '@':
+    if '@' in text[0]:
         param_dict["advanced"] = True
         text = text[1:]
 
@@ -37,12 +37,17 @@ def parse_param_to_dict(found_parameters, text, input_file, param_type):
 
     # If this parameter doesn't have a description, we have full name!
     if '(' not in text:
-        param_dict["name"] = text
+        param_dict["name"] = text.strip()
         text_finished = True
     # If this parameter *does* have a description. record the name
     else:
-        param_dict["name"] = text[0:text.find('(')]
-        text = text[text.find('(')+1:text.rfind(')')].replace(';', ',')
+        # If this is a properly formatted description with a )
+        if ')' in text:
+            description = text[text.find('('):text.rfind(')')+1].strip()
+        else:
+            description = text[text.find('('):].strip()
+        param_dict["name"] = text.replace(description, '').strip()
+        text = description.replace('(', '').replace(')', '').strip()
 
     # Record we've found it
     found_parameters.append(param_dict["name"])
@@ -66,7 +71,7 @@ def parse_param_to_dict(found_parameters, text, input_file, param_type):
     for key, value in bool_types.items():
         # Check to see if this is a de-facto boolean
         if key in text.lower():
-            param_dict["type"] = value
+            param_dict["type"] = value.strip()
             param_dict.pop("values", None)
             param_dict.pop("unit", None)
             text_finished = True
@@ -79,15 +84,15 @@ def parse_param_to_dict(found_parameters, text, input_file, param_type):
         for key, value in unit_types.items():
             # Is the description a unit?
             if key in text.lower():
-                param_dict["unit"] = value
+                param_dict["unit"] = value.strip()
                 text_finished = True
                 break
         if text_finished:
             return False
-        else:
-            # If it's not a unit, and not an enumerator, it's just a description
-            param_dict["description"] = text+'\n'
-            return False
+
+        # If it's not a unit, and not an enumerator, it's just a description
+        param_dict["description"] = text.strip()+'\n'
+        return False
 
     # This must be an enumerated list
     param_dict["type"] = "Enum (Int)"
@@ -103,19 +108,20 @@ def parse_param_to_dict(found_parameters, text, input_file, param_type):
             else:
                 value, key = pair.strip(')').split('(')
             try:
-                enum_dict[int(key)] = value
+                enum_dict[int(key)] = value.strip()
             except ValueError:
-                enum_dict[key] = value
+                enum_dict[key] = value.strip()
         param_dict["values"] = enum_dict
     except:
         # If it can't be automatically done, dump it as a string for manual editing
-        param_dict["values"] = text
+        param_dict["values"] = text.strip()
     return False
 
 
 # Set up input and output folders. Requires PYTHON environment variable.
 input_folder = os.path.join(os.environ["PYTHON"], "source")
 output_folder = os.path.join(os.environ["PYTHON"], "docs", "parameters")
+output_old_folder = os.path.join(os.environ["PYTHON"], "docs", "parameters", "old")
 
 # Do not generate documentation for inputs in files containing these substrings
 blacklist = ["py_wind", "plot_roche", "py_grid", "synonyms.c"]
@@ -130,21 +136,19 @@ param_types = {"get_spectype": "Int",
                "rdline": "Line"}
 # Types of unit in the description, and what to write in the output.
 # The first matching entry will be applied (e.g. msol/yr before msol).
-unit_types = OrderedDict([
-                ("cm", "cm"),
-                ("ergs/s", "ergs/s"),
-                ("ev", "eV"),
-                ("deg", 'Degrees'),
-                ("msol/yr", u"M☉/year"),
-                ("msol", u"M☉"),
-                ("hr", "Hours"),
-                ("r_star", "co.radius"),
-                ("rstar", "co.radius"),
-                ("wd_rad", "co.radius"),
-                ("r_g", "co.gravitational_radius"),
-                ("cgs", "cgs"),
-                ("vescap", "Escape velocity")
-                ])
+unit_types = OrderedDict([("cm", "cm"),
+                          ("ergs/s", "ergs/s"),
+                          ("ev", "eV"),
+                          ("deg", 'Degrees'),
+                          ("msol/yr", u"M☉/year"),
+                          ("msol", u"M☉"),
+                          ("hr", "Hours"),
+                          ("r_star", "co.radius"),
+                          ("rstar", "co.radius"),
+                          ("wd_rad", "co.radius"),
+                          ("r_g", "co.gravitational_radius"),
+                          ("cgs", "cgs"),
+                          ("vescap", "Escape velocity")])
 # Ways boolean variables are shown in the description and their types.
 bool_types = {"1=yes": "Boolean (1/0)",
               "y=1": "Boolean (1/0)",
@@ -157,11 +161,17 @@ try:
     os.makedirs(os.fsencode(output_folder))
 except OSError:
     pass
+try:
+    os.makedirs(os.fsencode(output_old_folder))
+except OSError:
+    pass
+
 
 existing_documentation = []
 for filename in os.listdir(output_folder):
-    parameter = os.path.splitext(filename)[0]
-    existing_documentation.append(parameter)
+    parameter = os.path.splitext(filename)
+    if 'yaml' in parameter[1]:
+        existing_documentation.append(parameter[0])
 
 # Build a list of input files
 input_files = []
@@ -182,7 +192,7 @@ for input_file in input_files:
     while not file_object.closed:
         # Whilst this file is open, read a line in to find a new parameter.
         line = file_object.readline()
-        if line is "":
+        if not line:
             # If it's "", this is the end of the file. Close it.
             file_object.close()
             break
@@ -197,12 +207,29 @@ for input_file in input_files:
             # Line of actual c code
             for key, value in param_types.items():
                 if key in line.lower():
-                    # We've found a rdpar line with a readable entry, now let's build the text to parse
+                    # We've found a rdpar line with a readable entry,
+                    # now let's build the text to parse
                     while ";" not in line:
                         # If this line is spread across multiple lines:
-                        # Remove trailing '\n' and staple next line onto it
-                        line = line[:-1]
                         line += file_object.readline()
+
+                    # Remove any block comments from the line
+                    while '/*' in line:
+                        comment_start = line.find('/*')
+                        comment_finish = line.find('*/')+1
+                        comment = line[comment_start:comment_finish]
+                        line = line.replace(comment, '')
+
+                    # Remove any single-line comments
+                    while '//' in line:
+                        comment_start = line.find('//')
+                        comment_finish = line.find('\n', comment_start)+1
+                        comment = line[comment_start:comment_finish]
+                        line = line.replace(comment, '')
+
+                    # Remove any linebreaks
+                    line = line.replace('\n', '')
+
                     if '(' not in line or '"' not in line:
                         # This isn't anything useful
                         continue
@@ -212,13 +239,24 @@ for input_file in input_files:
                     parse_param_to_dict(found_parameters=found_parameters, text=text,
                                         input_file=input_file, param_type=key)
 
-print("Documentation for parameters that no longer exist:")
-for param in sorted(list(set(existing_documentation) - set(found_parameters)), key=lambda s: s.lower()):
-    print(" - {}".format(param))
-print("New, previously undocumented parameters:")
-for param in sorted(list(set(found_parameters) - set(existing_documentation)), key=lambda s: s.lower()):
-    print(" - {}".format(param))
-sys.exit(1)
+# Intersections of documentation
+deprecated_documentation = sorted(list(set(existing_documentation) - set(found_parameters)),
+                                  key=lambda s: s.lower())
+new_documentation = sorted(list(set(found_parameters) - set(existing_documentation)),
+                           key=lambda s: s.lower())
+
+# If we're not running in write mode
+if len(sys.argv) is 1:
+    print("Documentation for parameters that no longer exist:")
+    for param in deprecated_documentation:
+        print(" - {}".format(param))
+    print("New, previously undocumented parameters:")
+    for param in new_documentation:
+            print(" - {}".format(param))
+    print("Rerun with:\n" +
+          "'-w' to move old parameters to 'parameters/old' and write new files.\n" +
+          "'-p' to print new files to screen.")
+    sys.exit(1)
 
 
 # From https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
@@ -232,15 +270,16 @@ def should_use_block(value):
     Returns:
         bool: Whether or not this is a multi-line string
     """
-    for c in u"\u000a\u000d\u001c\u001d\u001e\u0085\u2028\u2029":
-        if c in value:
+    for character in u"\u000a\u000d\u001c\u001d\u001e\u0085\u2028\u2029":
+        if character in value:
             return True
     return False
 
 
 def my_represent_scalar(self, tag, value, style=None):
     """
-    Replacement 'representer' for strings that intelligently switches between single- and multi-line output
+    Replacement 'representer' for strings that intelligently switches between single- and
+    multi-line output
 
     Args:
         tag (string): YAML tag
@@ -279,9 +318,22 @@ yaml.representer.BaseRepresenter.represent_scalar = my_represent_scalar
 yaml.add_representer(OrderedDict, dict_representer)
 
 # Output to file and print to screen
+if '-p' in sys.argv[1:]:
+    print("Printing new documentation to screen...\n")
+if '-w' in sys.argv[1:]:
+    print("Writing out...")
+    for filename in deprecated_documentation:
+        original_path = os.path.join(output_folder, '{}.yaml'.format(filename))
+        moved_path = os.path.join(output_old_folder, '{}.yaml'.format(filename))
+        os.rename(original_path, moved_path)
+
+
 for key, value in output_dict.items():
-    with open(os.path.join(output_folder, "{}.yaml".format(key)), "w") as file_object:
-        # For each key, open a yaml file and write to it.
-        print("File: {}/{}.yaml".format(output_folder, key))
-        # print(yaml.dump(value, default_flow_style=False, allow_unicode=True))
-        yaml.dump(value, file_object, default_flow_style=False, allow_unicode=True)
+    print("File: {}/{}.yaml".format(output_folder, key))
+    if '-p' in sys.argv[1:] and key in new_documentation:
+        print(yaml.dump(value, default_flow_style=False, allow_unicode=True))
+
+    if '-w' in sys.argv[1:] and key in new_documentation:
+        with open(os.path.join(output_folder, "{}.yaml".format(key)), "w") as file_object:
+            # For each key, open a yaml file and write to it.
+            yaml.dump(value, file_object, default_flow_style=False, allow_unicode=True)
