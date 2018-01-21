c9da1d38f68835374bd812af4d2ae933c0c38a3c
Sam Mangham <mangham@gmail.com>
2018-01-21 14:40:55 +0000

    Added levels and children

--- docs/reverb.rst
+++ docs/reverb.rst
@@ -1,28 +1,54 @@
 
-=====
+====================
 Reverb
-=====
+====================
 
-reverb.angle_bins
-==============================
+reverb.type
+====================
+Whether to perform reverberation mapping. Reverberation mapping tracks the
+path of photons emitted in the simulation as they travel through the geometry,
+assuming that any delays from recombination etc. are negligible and all delays
+are due to light travel time. For each final spectrum, all contributing
+photons are output to a '.delay_dump' file that can then be processed using
+our 'tfpy' Python (no relation) library.
 
-Used when generating 3d .vtk output files for visualisation. Sets the number
-of angle bins used in the output. Aesthetic only; bigger makes prettier meshes
-with larger filesizes.
+**Type:** Enum (Int)
 
-**Type:** Int
+**Values:**
 
-**Value:** Greater than 0
+0. **Off**
 
-**Parent(s):**
-  *reverb.mode:* 1, 3
+1. **Simple 'photon' mode**
+   
+   Each photon is assigned an initial path based on its distance from the
+   central source (assuming emission in the disk and wind is correlated with
+   emission from the CO).
+
+2. **Wind mode**
+   
+   CO photons are assigned paths as in Photon mode, disk photons are assigned
+   paths as set by the reverb.disk_type parameter. Photons generated in the
+   wind are assigned a path based on the *distribution* of paths of photons
+   that have contributed to continuum absorption in that cell.
+
+3. **Macro-atom mode**
+   
+   This works as wind mode, but for a number of specified macro-atom lines
+   paths are tracked for those photons who cause a deexcitation into a given
+   line. When a photon is emitted in one of those lines, the path is drawn from
+   that specific distribution. This distribution is build up not just from the
+   last cycle of the simulation, but from all cycles after the wind achieves
+   >90% convergence. This is necessary as some lines are poorly-sampled.
+   
+   This mode gives pretty much identical results to wind mode, but at least we
+   made it to check rather than just assuming it would be fine.
 
 
 **File:** setup_reverb.c
 
-reverb.disk_type
-==============================
 
+reverb.disk_type
+--------------------
 Setting for how photons generated in the disk are treated when generating path
 distributions for wind cells.
 
@@ -65,70 +91,9 @@ distributions for wind cells.
 
 **File:** setup_reverb.c
 
-reverb.dump_cell
-==============================
-
-Position for a cell, listed as a pair of R:Z coordinates. Will accept any
-position that falls within a grid, will error out on ones that don't. This can
-be slightly awkward and you may want to run a quick test then use py_wind to
-idenfity where wind locations are.
-
-**Type:** Float:Float
-
-
-**Unit:** cm:cm
-
-
-**Value:** >0:>0
-
-
-**Parent(s):**
-  *reverb.dump_cells:* Greater than 0
-
-
-**File:** setup_reverb.c
-
-reverb.dump_cells
-==============================
-
-Number of cells to dump. When dumping the path distribution info for a range
-of cells, this specifies the number of lines of reverb.dump_cell that will be
-provided.
-
-**Type:** Int
-
-**Value:** 0 or N
-
-**Parent(s):**
-  *reverb.visualisation:* 2, 3
-
-
-**File:** setup_reverb.c
-
-reverb.filter_line
-==============================
-
-Line number of one line to include in the output .delay_dump file. This is
-the python internal line number. It can be found using either the macro-atom
-mode (which prints out the line number once it's found one) or by doing an
-exploratory run with reverb.filter_lines = -1, then looking through the delay
-dump file for photons of the right wavelength to see what their line is. This
-should almost certainly be changed to be specified using a species and
-wavelength!
-
-**Type:** Int
-
-**Value:** Any valid line index
-
-**Parent(s):**
-  *reverb.filter_lines:* Greater than 0
-
-
-**File:** setup_reverb.c
 
 reverb.filter_lines
-==============================
-
+--------------------
 Whether or not to filter any lines out of the output file. This is used to keep output
 file sizes down, and avoid them overwhelming the user.
 
@@ -160,9 +125,49 @@ N. **Filter lines**
 
 **File:** setup_reverb.c
 
-reverb.matom_line
-==============================
 
+reverb.filter_line
+^^^^^^^^^^^^^^^^^^^^
+Line number of one line to include in the output .delay_dump file. This is
+the python internal line number. It can be found using either the macro-atom
+mode (which prints out the line number once it's found one) or by doing an
+exploratory run with reverb.filter_lines = -1, then looking through the delay
+dump file for photons of the right wavelength to see what their line is. This
+should almost certainly be changed to be specified using a species and
+wavelength!
+
+**Type:** Int
+
+**Value:** Any valid line index
+
+**Parent(s):**
+  *reverb.filter_lines:* Greater than 0
+
+
+**File:** setup_reverb.c
+
+
+reverb.matom_lines
+--------------------
+Number of macro-atom lines to track paths for individually. This many
+reverb.matom_line entries are required, and the line associated with each has
+the path of photons deexciting into it recorded in its own array. Note: This
+doesn't give rise to any noticable differences to the pure wind mode in most
+simulations.
+
+**Type:** Int
+
+**Value:** 0 or N
+
+**Parent(s):**
+  *reverb.type:* 3
+
+
+**File:** setup_reverb.c
+
+
+reverb.matom_line
+^^^^^^^^^^^^^^^^^^^^
 Specifies a line associated with a given macro-atom transition. The species
 and transition involved are specified. The internal line associated with this
 transition will be printed to standard-out for use when processing outputs. A
@@ -180,27 +185,9 @@ line is specified as Element:Ion:Upper level:Lower level.
 
 **File:** setup_reverb.c
 
-reverb.matom_lines
-==============================
-
-Number of macro-atom lines to track paths for individually. This many
-reverb.matom_line entries are required, and the line associated with each has
-the path of photons deexciting into it recorded in its own array. Note: This
-doesn't give rise to any noticable differences to the pure wind mode in most
-simulations.
-
-**Type:** Int
-
-**Value:** 0 or N
-
-**Parent(s):**
-  *reverb.type:* 3
-
-**File:** setup_reverb.c
 
 reverb.path_bins
-==============================
-
+--------------------
 Number of bins for photon paths. Reverb modes that record the distribution of
 path lengths in every wind cell bin them in this number of bins. Bins are
 logarithmically spaced between the minimum scale in the system (the smallest
@@ -219,53 +206,9 @@ bin boundaries show up in the TF.
 
 **File:** setup_reverb.c
 
-reverb.type
-==============================
-
-Whether to perform reverberation mapping. Reverberation mapping tracks the
-path of photons emitted in the simulation as they travel through the geometry,
-assuming that any delays from recombination etc. are negligible and all delays
-are due to light travel time. For each final spectrum, all contributing
-photons are output to a '.delay_dump' file that can then be processed using
-our 'tfpy' Python (no relation) library.
-
-**Type:** Enum (Int)
-
-**Values:**
-
-0. **Off**
-
-1. **Simple 'photon' mode**
-   
-   Each photon is assigned an initial path based on its distance from the
-   central source (assuming emission in the disk and wind is correlated with
-   emission from the CO).
-
-2. **Wind mode**
-   
-   CO photons are assigned paths as in Photon mode, disk photons are assigned
-   paths as set by the reverb.disk_type parameter. Photons generated in the
-   wind are assigned a path based on the *distribution* of paths of photons
-   that have contributed to continuum absorption in that cell.
-
-3. **Macro-atom mode**
-   
-   This works as wind mode, but for a number of specified macro-atom lines
-   paths are tracked for those photons who cause a deexcitation into a given
-   line. When a photon is emitted in one of those lines, the path is drawn from
-   that specific distribution. This distribution is build up not just from the
-   last cycle of the simulation, but from all cycles after the wind achieves
-   >90% convergence. This is necessary as some lines are poorly-sampled.
-   
-   This mode gives pretty much identical results to wind mode, but at least we
-   made it to check rather than just assuming it would be fine.
-
-
-**File:** setup_reverb.c
 
 reverb.visualisation
-==============================
-
+--------------------
 Which type of visualisation to output, if any. Reverb modes that keep arrays
 of photon paths per cell can output them either as averages in a 3d model, or
 as a selection of flat text files with full bin-by-bin breakdowns. Useful for
@@ -296,4 +239,62 @@ diagnostics.
 
 
 **File:** setup_reverb.c
+
+
+reverb.angle_bins
+^^^^^^^^^^^^^^^^^^^^
+Used when generating 3d .vtk output files for visualisation. Sets the number
+of angle bins used in the output. Aesthetic only; bigger makes prettier meshes
+with larger filesizes.
+
+**Type:** Int
+
+**Value:** Greater than 0
+
+**Parent(s):**
+  *reverb.visualisation:* 1, 3
+
+
+**File:** setup_reverb.c
+
+
+reverb.dump_cells
+^^^^^^^^^^^^^^^^^^^^
+Number of cells to dump. When dumping the path distribution info for a range
+of cells, this specifies the number of lines of reverb.dump_cell that will be
+provided.
+
+**Type:** Int
+
+**Value:** 0 or N
+
+**Parent(s):**
+  *reverb.visualisation:* 2, 3
+
+
+**File:** setup_reverb.c
+
+
+reverb.dump_cell
+^^^^^^^^^^^^^^^^^^^^
+Position for a cell, listed as a pair of R:Z coordinates. Will accept any
+position that falls within a grid, will error out on ones that don't. This can
+be slightly awkward and you may want to run a quick test then use py_wind to
+idenfity where wind locations are.
+
+**Type:** Float:Float
+
+
+**Unit:** cm:cm
+
+
+**Value:** >0:>0
+
+
+**Parent(s):**
+  *reverb.dump_cells:* Greater than 0
+
+
+**File:** setup_reverb.c
+
 
--- py_progs/autogenerate_rtd_pages.py
+++ py_progs/autogenerate_rtd_pages.py
@@ -1,7 +1,19 @@
 import yaml
 import os
 import sys
-from collections import OrderedDict
+
+
+def write_header_by_level(output_file, string, level=0):
+    """
+    Writes an appropriate header out
+    """
+    if level is 0:
+        output_file.write("{}\n====================\n".format(string))
+    elif level is 1:
+        output_file.write("{}\n--------------------\n".format(string))
+    else:
+        output_file.write("{}\n^^^^^^^^^^^^^^^^^^^^\n".format(string))
+    return
 
 
 def write_str_indent(output_file, string, indent="  "):
@@ -16,109 +28,143 @@ def write_str_indent(output_file, string, indent="  "):
     return
 
 
-def output_parameter(output_file, parameter):
+def output_parameter(output_file, parameter, level=0):
+    """
+    Output a parameter to file
+    """
+    write_header_by_level(output_file, parameter['name'], level)
+
+    type_file.write("{}\n".format(parameter['description']))
+
+    if parameter.get('type'):
+        type_file.write("**Type:** {}\n\n".format(parameter['type']))
+    if parameter.get('unit'):
+        type_file.write("**Unit:** {}\n\n".format(parameter['unit']))
+
+    if parameter.get('values'):
+        if isinstance(parameter['values'], dict):
+            type_file.write("**Values:**\n\n")
+            for key, value in parameter['values'].items():
+                if isinstance(value, str):
+                    write_str_indent(type_file, "{}. {}".format(key, value.strip()), indent="   ")
+                elif isinstance(value, list):
+                    list_text = ', '.join([str(x) for x in value])
+                    write_str_indent(type_file, "{}. {}".format(key, list_text, indent="   "))
+                else:
+                    type_file.write("{}\n. {}\n".format(key, value))
+
+        elif isinstance(parameter['values'], list):
+            # If this is a list of values, write each out as a bullet-point
+            type_file.write("**Values:**\n\n")
+            for value in parameter['values'].items():
+                write_str_indent(type_file, "* {}".format(value), indent="  ")
+
+        else:
+            type_file.write("**Value:** {}\n".format(parameter['values']))
+        type_file.write("\n")
+
+    if parameter.get('parent'):
+        if isinstance(parameter['parent'], dict):
+            type_file.write("**Parent(s):**\n")
+            for key, value in parameter['parent'].items():
+                if isinstance(value, str):
+                    write_str_indent(type_file, "  *{}:* {}".format(key, value), indent="    ")
+                elif isinstance(value, list):
+                    list_text = ', '.join([str(x) for x in value])
+                    write_str_indent(type_file, "  *{}:* {}".format(key, list_text, indent="    "))
+                else:
+                    type_file.write("  *{}:* {}\n\n".format(key, value))
+            type_file.write("\n")
+
+        elif isinstance(parameter['parent'], list):
+            # If this is a list of parents, write each out as a bullet-point
+            type_file.write("**Parent(s):**\n")
+            for value in parameter['parent'].items():
+                write_str_indent(type_file, "* {}".format(value), indent="  ")
+        else:
+            type_file.write("**Parent(s):** {}\n\n".format(parameter['parent']))
+
+    type_file.write("**File:** {}\n\n\n".format(parameter['file']))
+
+    # Go through all the children and output them
+    if parameter.get('children'):
+        for child_name, child in parameter['children'].items():
+            output_parameter(output_file, child, level+1)
     return
 
 
 input_folder = os.path.join(os.environ["PYTHON"], "docs", "parameters")
 output_folder = os.path.join(os.environ["PYTHON"], "docs")
 dox_root = {}
+dox_all = {}
 
+# First, read the directory to get all the input files
 input_files = []
 for filename in os.listdir(input_folder):
     # For each file in the input folder
-    if filename.endswith(".yaml"):
+    if filename.endswith(".yaml") and 'reverb' in filename:
         # This is a valid input file
         input_files.append(filename)
     else:
         continue
 
+# Second, read in all parameters from the input files
 for input_file in input_files:
     with open(os.path.join(input_folder, input_file), "r") as file_object:
         # For each key, open a yaml file and load from it.
         try:
             parameter = yaml.load(file_object)
             parameter_name = parameter['name']
-            parameter_type = parameter['name'].split('.')[0]
 
-            if parameter_type not in dox_root:
-                dox_root[parameter_type] = {}
-            dox_root[parameter_type][parameter_name] = parameter
+            # Get the type from the name (e.g. reverb.mode and set no children)
+            parameter_type = parameter['name'].split('.')[0]
+            parameter['root_type'] = parameter_type.strip()
+            parameter['children'] = {}
+
+            # Register this on the full list of all parameters
+            dox_all[parameter_name] = parameter
 
         except yaml.YAMLError as error:
             print(error)
 
-dox_unstructured = dox_root.copy()
 dox_structured = {}
-for key in dox_unstructured.keys():
-    dox_structured[key] = {}
 
-for parameter_type, parameters in dox_unstructured.items():
-    for parameter_name in list(parameters.keys()):
-        parameter = parameters[parameter_name]
-        if parameter.get('parent'):
-            for parent_name, value2 in parameter['parent'].items():
-                # If this parameter has a parent parameter that is *also* in this file
-                parent_type = parent_name.split('.')[0]
-                if parent_type in parameter_type:
+# For each possible root type, set up a root dictionary for it
+for parameter_name, parameter in dox_all.items():
+    dox_structured[parameter['root_type']] = {}
+
+# We want to build a tree with children etc. to respect parentage
+# Now, for the unstructured list of all parameters
+for parameter_name, parameter in dox_all.items():
+    # For each parameter in the list of all parameters, check its parents
+    parameter_type = parameter['root_type']
+    if parameter.get('parent'):
+        # If it has parents
+        found_parent = False
+        for parent_name, value in parameter['parent'].items():
+            # Check each parent.
+            if parent_name in dox_all.keys():
+                # If it's parent is another parameter (i.e. in .keys())
+                if parameter_type == dox_all[parent_name]['root_type']:
+                    # If both the parameter and its parents are in the same class
+                    # We assign this parameter to the children's list of its parent
+                    dox_all[parent_name]["children"][parameter_name] = parameter
+                    # And then 'continue' on, skipping other parents
+                    found_parent = True
                     continue
-                else:
-                    dox_structured[parameter_type][parent_name] = parameter
-                    del dox_unstructured[parameter_type][parameter_name]
 
-for parameter_type, type_parameters in dox_root.items():
+        if not found_parent:
+            # If we didn't find the parameter's parent elsewhere,
+            # then add it to the root.
+            dox_structured[parameter_type][parameter_name] = parameter
+
+    else:
+        # If it has no parents, add it to the top layer
+        dox_structured[parameter_type][parameter_name] = parameter
+
+# Go through the parameter tree and write it out
+for parameter_type, type_parameters in dox_structured.items():
     with open(os.path.join(output_folder, "{}.rst".format(parameter_type)), 'w') as type_file:
-        type_file.write("\n=====\n{}\n=====\n\n".format(parameter_type.title()))
+        type_file.write("\n====================\n{}\n====================\n\n".format(parameter_type.title()))
         for parameter_name, parameter in type_parameters.items():
-            type_file.write("{}\n==============================\n\n".format(parameter['name']))
-            type_file.write("{}\n".format(parameter['description']))
-
-            if parameter.get('type'):
-                type_file.write("**Type:** {}\n\n".format(parameter['type']))
-            if parameter.get('unit'):
-                type_file.write("**Unit:** {}\n\n".format(parameter['unit']))
-
-            if parameter.get('values'):
-                if isinstance(parameter['values'], dict):
-                    type_file.write("**Values:**\n\n")
-                    for key, value in parameter['values'].items():
-                        if isinstance(value, str):
-                            write_str_indent(type_file, "{}. {}".format(key, value), indent="   ")
-                        elif isinstance(value, list):
-                            list_text = ', '.join([str(x) for x in value])
-                            write_str_indent(type_file, "{}. {}".format(key, list_text, indent="   "))
-                        else:
-                            type_file.write("{}\n. {}\n".format(key, value))
-
-                elif isinstance(parameter['values'], list):
-                    # If this is a list of values, write each out as a bullet-point
-                    type_file.write("**Values:**\n\n")
-                    for value in parameter['values'].items():
-                        write_str_indent(type_file, "* {}".format(value), indent="  ")
-
-                else:
-                    type_file.write("**Value:** {}\n".format(parameter['values']))
-                type_file.write("\n")
-
-            if parameter.get('parent'):
-                if isinstance(parameter['parent'], dict):
-                    type_file.write("**Parent(s):**\n")
-                    for key, value in parameter['parent'].items():
-                        if isinstance(value, str):
-                            write_str_indent(type_file, "  *{}:* {}".format(key, value), indent="    ")
-                        elif isinstance(value, list):
-                            list_text = ', '.join([str(x) for x in value])
-                            write_str_indent(type_file, "  *{}:* {}".format(key, list_text, indent="    "))
-                        else:
-                            type_file.write("  *{}:* {}\n".format(key, value))
-
-                elif isinstance(parameter['parent'], list):
-                    # If this is a list of parents, write each out as a bullet-point
-                    type_file.write("**Parent(s):**\n")
-                    for value in parameter['parent'].items():
-                        write_str_indent(type_file, "* {}".format(value), indent="  ")
-                else:
-                    type_file.write("**Parent(s):** {}\n".format(parameter['parent']))
-                type_file.write("\n")
-
-            type_file.write("**File:** {}\n\n".format(parameter.get('file')))
+            output_parameter(type_file, parameter, level=0)
