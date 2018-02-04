ecfbad3ed13775d4e9d76d3d1850aec4594e28bb
Sam Mangham <mangham@gmail.com>
2018-02-04 16:53:14 +0000

    Added argument parsing, subdivided a lot of bits

--- py_progs/doxygen.py
+++ py_progs/doxygen.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-
-
 '''
                     Space Telescope Science Institute
 
@@ -51,7 +49,6 @@ Notes:
     blocks.  ksl suspects this should be systemetized
 
 
-
 History:
 
 180123 ksl Coding begun
@@ -61,8 +58,6 @@ History:
 '''
 
 import sys
-from astropy.io import ascii
-import numpy
 import subprocess
 
 
@@ -94,21 +89,183 @@ def is_installed(program):
     raise(OSError("Executable '{}' could not be found!".format(program)))
 
 
-def read_file(filename,char=''):
+def read_file(filename):
     '''
     Read a file
 
+    Args:
+        filename (str): Name of the file to read
+    Returns:
+        list: Lines within the file
     '''
 
     try:
-        f=open(filename,'r')
-        xlines=f.readlines()
+        f = open(filename, 'r')
+        xlines = f.readlines()
         f.close()
-    except IOError :
-        print ("The file %s does not exist" % filename)
+    except IOError as e:
+        print("The file %s does not exist" % filename)
         return []
 
     return xlines
+
+
+def write_header(function):
+    """
+
+    Returns:
+        String: The header to be written to file
+    """
+    header = module_string_start.replace(r'%s', r'{}').replace(r'???', r'{}')\
+        .format(function['name'], function['synopsis'].replace('\n', '\n * '))
+
+    for name, argument in function['arguments'].items():
+        header += module_string_param.replace(r'%s', r'{}').replace(r'???', r'{}')\
+            .format(argument['type'], argument['name'],
+                    argument['description'].replace('\n', '\n * '))
+
+    header += module_string_end.replace(r'%s', r'{}').replace(r'???', r'{}')\
+        .format(function['returns'].replace('\n', '\n * '),
+                function['description'].replace('\n', '\n * '),
+                function['notes'].replace('\n', '\n * '))
+    return header
+
+
+def parse_header_block(header, title, strip=True, default=None):
+    """
+    Given a list of strings describing a function header, extracts the specified block.
+
+    Does so by iterating over lines in the array, stapling them together into a single string.
+
+    Arguments:
+        header (list): List of strings making up the header
+        title (string): Block title
+
+    Returns:
+        list: List of string making up the block
+    """
+    titles = ['Arguments:', 'Returns:', 'Notes:', 'Description:',
+              'History:', 'Synopsis:', '**********']
+    block = None
+    for line in header:
+        # For each line in the header
+        if not block and title in line:
+            # We've found the start of the arguments block
+            block = line[line.find(title)+len(title):]
+        elif block:
+            if any(title in line for title in titles):
+                # We've reached the start of another block
+                if not block.strip():
+                    # If this is just a block of whitespace with no text
+                    return default
+                elif strip:
+                    # If we're stripping whitespace out
+                    return block.strip()
+                else:
+                    # If we're not, well, leave it be
+                    return block
+            else:
+                # We're still going through the block, so append this
+                block += line
+    # We never found the title, so give up and return the default
+    return default
+
+
+def parse_header(header, func_dict):
+    """
+    Given a list of strings describing a function header, parses them into info.
+
+    Arguments:
+        header (list): List of strings making up this header
+        func_dict (dict): Dictionary describing this functions assembled by cproto
+
+    Returns:
+        dict: Updated func_dict. Could just edit in place, but it's more pythonic?
+    """
+    # Seek through to find the start of the arguments block
+    block = parse_header_block(header, 'Arguments:', strip=False)
+
+    print('Parsing header for {}'.format(func_dict['name']))
+
+    if block:
+        # If there is an argument block
+        block_test = block.replace('\t', ' ').lower()
+
+        for name, parameter in func_dict['arguments'].items():
+            # We want to find this name in the line. BUT! It could be, like 'a'. That's no good.
+            # So we need to find a string like ' a; blah' or ' a: blah' or ' a blah'
+            # Made a bit more messy given there's some listed as 'a,b,c'
+
+            name_test = ' {};'.format(name).lower()
+            name_index = block_test.find(name_test)
+            if name_index == -1:
+                name_test = ' {}:'.format(name).lower()
+                name_index = block_test.find(name_test)
+            if name_index == -1:
+                name_test = ' {} '.format(name).lower()
+                name_index = block_test.find(name_test)
+            if name_index == -1:
+                name_test = ',{};'.format(name).lower()
+                name_index = block_test.find(name_test)
+            if name_index == -1:
+                name_test = ',{}:'.format(name).lower()
+                name_index = block_test.find(name_test)
+            if name_index == -1:
+                name_test = ',{} '.format(name).lower()
+                name_index = block_test.find(name_test)
+
+            if name_index > -1:
+                # If we *did* find the substring, add the length of the variable name to it
+                name_index += len(name_test)
+                comma_index = block.find(',', name_index)
+                break_index = block.find('\n', name_index)
+                if comma_index > -1 and break_index > -1:
+                    if comma_index < break_index:
+                        description = block[name_index:comma_index].strip()
+                    else:
+                        description = block[name_index:break_index].strip()
+                elif comma_index > -1:
+                    description = block[name_index:comma_index].strip()
+                elif break_index > -1:
+                    description = block[name_index:break_index].strip()
+                else:
+                    description = block[name_index:].strip()
+
+                # Did we ever actually find a description?
+                if description:
+                    parameter['description'] = description
+
+    func_dict['synopsis'] = parse_header_block(header, 'Synopsis:', default='??? SYNOPSIS ???')\
+        .replace(func_dict['name'], '').strip()
+    func_dict['description'] = parse_header_block(header, 'Description:',
+                                                  default='??? DESCRIPTION ???')
+    func_dict['returns'] = parse_header_block(header, 'Returns:', default='??? RETURNS ???')
+    func_dict['notes'] = parse_header_block(header, 'Notes:', default='??? NOTES ???')
+    func_dict['history'] = parse_header_block(header, 'History:', default='??? HISTORY ???')
+    return func_dict
+
+
+def split_definition(definition):
+    """
+    Taking a definition as 'float* blah', splits it into the name and type
+
+    Args:
+        definition (str): String containing definition (function or variable)
+
+    Returns:
+        tuple: Name and type of the definition
+    """
+    last_space = definition.rfind(' ')
+    last_pointer = definition.rfind('*')
+    # We need to see if this variable is type name or type* name or type *name
+    if last_pointer > last_space:
+        # If there's a pointer assignment, use that in the type
+        definition_name = definition[last_pointer+1:].strip()
+        definition_type = definition[:last_pointer+1].strip()
+    else:
+        definition_name = definition[last_space+1:].strip()
+        definition_type = definition[:last_space].strip()
+    return (definition_name, definition_type)
 
 
 def get_modules(filename='emission.c'):
@@ -118,33 +275,54 @@ def get_modules(filename='emission.c'):
     returned into something which can be incorporated
     into a search
 
+    Args:
+        filename (str): Name of the file to be read
+
+    Returns:
+        List of records and dict of records
     '''
-    proc=subprocess.Popen('cproto %s ' % filename,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    stdout,stderr=proc.communicate()
 
-    stdout=stdout.decode()
-    stdout=stdout.split('\n')
+    from collections import OrderedDict
 
-    # print(stdout)
+    proc = subprocess.Popen('cproto %s ' % filename, shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    stdout = stdout.decode().split('\n')
 
-    records=[]
-    i=1
-    while i<len(stdout)-1:
-        z=stdout[i].replace('(',' ')
-        z=z.replace(')',' ')
-        z=z.replace(',',' ')
-        z=z.replace(';',' ')
-        words=z.split()
+    records_dict = OrderedDict()
+    for line in stdout[1:]:
+        if line and not ('bad character' in line or 'syntax error' in line):
+            # Find the substring of arguments in 'type func_name(type arg1, type arg2);'
+            function = {'arguments': OrderedDict()}
+            function['name'], function['type'] = split_definition(line[:line.find('(')])
+            argument_lines = line[line.find('(')+1:line.find(')')].split(',')
+            if 'void' not in argument_lines:
+                for argument_line in argument_lines:
+                    argument = {'description': '???'}
+                    argument['name'], argument['type'] = split_definition(argument_line)
+                    function['arguments'][argument['name']] = argument
+
+            records_dict[function['name']] = function
+
+    records = []
+    i = 1
+    while i < len(stdout)-1:
+        z = stdout[i].replace('(', ' ')
+        z = z.replace(')', ' ')
+        z = z.replace(',', ' ')
+        z = z.replace(';', ' ')
+        words = z.split()
         # print(words)
-        one_record=[stdout[i]]+words
+        one_record = [stdout[i]] + words
         records.append(one_record)
         # records.append([stdout[i],words[0],words[1]])
 
-        i+=1
+        i += 1
 
-    return(records)
+    return (records, records_dict)
 
-file_string='''
+
+file_string = '''
 /***********************************************************/
 /** @file   %s
  * @Author ksl
@@ -156,28 +334,28 @@ file_string='''
  ***********************************************************/
 '''
 
-module_string_start='''
-
+module_string_start = '''
 /**********************************************************/
-/** @name       %s
-* @brief       %s
-*
+/** @name      %s
+ * @brief      %s
+ *
 '''
-module_string_param='''* @param [in]  %s  %s   ???
+module_string_param = ''' * @param [in] %s  %s   ???
 '''
-module_string_end='''* @return       %s
-*
-* %s
-*
-* @notes
-*
-* %s
-*
-***********************************************************/
+module_string_end = ''' * @return     %s
+ *
+ * %s
+ *
+ * @notes
+ *
+ * %s
+ *
+ **********************************************************/
 
 '''
 
-def doit(filename='emission.c',outputfile=''):
+
+def doit(filename='emission.c', outputfile=None):
     '''
     Do something magnificent
 
@@ -193,94 +371,101 @@ def doit(filename='emission.c',outputfile=''):
 
     '''
 
-    if outputfile=='':
-        outputfile='new_'+filename
+    if not outputfile:
+        outputfile = 'new_'+filename
 
-    lines=read_file(filename)
+    lines = read_file(filename)
 
-    if len(lines)==0:
-        return
+    if len(lines) == 0:
+        raise(EOFError('File {} has no lines!').format(filename))
 
-    modules=get_modules(filename)
+    modules, mod_dict = get_modules(filename)
 
     print('These are the functions found in the file\n')
     for one in modules:
         print(one[0])
     print('\n')
 
+    xlines = 0
+    i = 0  # Index for iterating over the array of lines
+    j = 0  # Index for the position in the header array
+    header_start = []  # Position of the header start lines
+    header_end = []    # Position of the header end lines
+    module_start = []  # Position of the module start lines
+    module_end = []    # Position of the module end lines
+    module_name = []   # Name of the module for each start-end
 
+    # This section scans through the file, looking for header starts and ends.
+    while i < len(lines)-1:
+        line = lines[i].strip()
+        found_header = False
+        found_module = False
 
-    xlines=0
-    i=0
-    j=0
-    header_start=[]
-    header_end=[]
-    module_start=[]
-    while i<len(lines)-1:
-        line=lines[i].strip()
-        # print(line)
-        header=False
-
-        if line[0:24]=='/***********************':
+        if line[0:24] == '/***********************':
             # print('This is the beginning of a header')
             header_start.append(i)
-            header=True
-        if line[0:20]=='********************':
+            header = True
+        elif line[0:20] == '********************':
             # print('Found the end of a header')
             header_end.append(i)
-            header=False
-        if header==False and j<len(modules):
-            # Look for the begining of a module
-            x=line.strip()
-            y=lines[i+1].strip()
+            header = False
+
+        elif not (found_header or found_module) and j < len(modules):
+            # To try to match this header to a module, we peek at pairs of lines
+            # As most of our functions are defined as 'type \n name (arguments)'
+            x = line.strip()
+            y = lines[i+1].strip()
             # print('test',x,y)
-            x=x.split()
-            y=y.split()
+            x = x.split()
+            y = y.split()
             # print('test2',x,'y',y)
-            if len(x)>0 and len(y)>0 and x[0]==modules[j][1] and y[0]==modules[j][2]:
+            if len(x) > 0 and len(y) > 0 and x[0] == modules[j][1] and y[0] == modules[j][2]:
+                # If we've found the first two lines of the module, we append the start line of it
+                # and its name to the lists
                 module_start.append(i)
-                # print('Found module start')
-                j+=1
+                module_name.append(y[0])
+                j += 1
+                found_module = True
 
-        i+=1
+        elif found_module and line[0] == '}':
+            # If we've found the start of a module, we want to know where it ends.
+            # If we find a line that starts with '}', we've found the end (almost certainly)
+            module_end.append(i)
+            found_module = False
 
-    print('line where header starts   :',header_start)
-    print('line_where header_ends     :',header_end)
-    print('lines where function_starts:',module_start)
+        i += 1
 
+    print('line where header starts   :', header_start)
+    print('line_where header_ends     :', header_end)
+    print('lines where function_starts:', module_start)
 
     # Now we need to try to match the current headers with the modules because
     # some may be missing
 
-    xmatch=[]
-    i=0
-    while i<len(modules):
-        xmatch.append(-1)
-        i+=1
+    # xmatch is the array used to cross-match the headers in the code and their associated modules
+    xmatch = [-1] * len(modules)
 
-    j=0
-    isep=20
-    while j<len(header_end):
-        k=0
-        while k<len(module_start):
-            if header_end[j]<module_start[k] and module_start[k]-header_end[j]<isep:
-                xmatch[k]=j
+    isep = 20
+    for header_index, header_line in enumerate(header_end):
+        # We iterate through the array of headers
+        for module_index, module_line in enumerate(module_start):
+            # If the start line of this module is after the end of the current header, *and*
+            # there's no other module between it and the header.
+            if header_line < module_line and header_start[header_index-1] < header_line:
+                xmatch[module_index] = header_index
                 break
-            k+=1
-        j+=1
 
-    print('xmatch betwen headers and functions',xmatch)
+    print('xmatch betwen headers and functions', xmatch)
 
-
-    x=open(outputfile,'w')
+    x = open(outputfile, 'w')
 
     x.write(file_string % (outputfile))
 
-    i=0
-    kk=0
-    while i<len(lines):
-        line=lines[i]
-        k=0
+    i = 0
+    kk = 0
+    while i < len(lines):
+        line = lines[i]
+        k = 0
         while k<len(header_end):
             if header_start[k]<=i and i<= header_end[k]:
                 line='//OLD '+line
@@ -403,8 +588,6 @@ def doit(filename='emission.c',outputfile=''):
             else:
                 notes_string=notes_string.replace('\n','\n*      ')
 
-
-
             kkk=2
             # print(proto_string)
             while kkk<len(proto_string):
@@ -413,24 +596,24 @@ def doit(filename='emission.c',outputfile=''):
             x.write(module_string_end % (return_string,description_string,notes_string))
             kk+=1
 
-
         x.write(line)
         i+=1
 
+    import json
+    for index, module in enumerate(mod_dict.values()):
+        # We iterate over the dictionary of modules, looking up what header range corresponds to
+        # them using the xmatch array, and passing that to parse_header.
+        if xmatch[index] > -1:
+            # If there *is* a header for this module, pass it the text
+            module = parse_header(lines[header_start[xmatch[index]]:header_end[xmatch[index]]], module)
+        else:
+            # Else, pass it a blank string and let it set up a default one
+            module = parse_header('', module)
 
-
-
-
-
-
+        with open('tempdox_{}.c'.format(module['name']), 'w') as outfile:
+            outfile.write(write_header(module))
 
     return
-
-
-
-
-
-
 
 
 # Next lines permit one to run the routine from the command line
@@ -439,9 +622,9 @@ if __name__ == "__main__":
 
     is_installed('cproto')
 
-    if len(sys.argv)==1 or sys.argv[1]=='-h':
+    if len(sys.argv) == 1 or sys.argv[1] == '-h':
         print(__doc__)
-    elif len(sys.argv)>1:
+    elif len(sys.argv) > 1:
         doit(sys.argv[1])
     else:
-        print ('usage: doxygen.py [-h] filename')
+        print('usage: doxygen.py [-h] filename')
