378ce2d84478e3190d3e8b711e18302111bed106
Sam Mangham <mangham@gmail.com>
2019-07-11 14:38:58 +0100

    Fix bug with being unable to catch shorter synonyms than orignal question length.

--- source/synonyms.c
+++ source/synonyms.c
@@ -232,7 +232,7 @@ is_input_line_synonym_for_question(question, input_line)
     if(!strncmp(new_names[synonym_index], question, question_name_length)) {
 
       // Does the 'old name' match the question on the input line?
-      if(!strncmp(old_names[synonym_index], input_line, question_name_length)) {
+      if(!strncmp(old_names[synonym_index], input_line, get_question_name_length(old_names[synonym_index]))) {
         // If the question on the input line matches the 'old name' for this question (i.e. the difference is 0)
         return(1);
       }
