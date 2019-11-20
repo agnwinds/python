85cfefc0dc4e4feaf665540d4bfd75d169878724
kslong <long@stsci.edu>
2019-11-20 16:56:26 -0500

    Increased MAXSCAT to 1000

--- source/python.h
+++ source/python.h
@@ -101,7 +101,7 @@ int NPHOT_MAX;                  /* The maximum number of photon bundles created 
 int NPHOT;                      /* The number of photon bundles created, defined in setup.c */
 
 #define NWAVE  			  10000 //This is the number of wavelength bins in spectra that are produced
-#define MAXSCAT 			500
+#define MAXSCAT 			1000
 
 /* Define the structures */
 
