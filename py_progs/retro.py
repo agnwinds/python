a404311d96577458a6041aef8b95d9ddb737f7ea
Knox S. Long <long@stsci.edu>
2021-02-20 17:43:49 -0600

    Fix to to retro.py

--- py_progs/retro.py
+++ py_progs/retro.py
@@ -83,6 +83,10 @@ from scipy.signal import convolve
 home=os.getcwd()
 
 
+FAILURE=True
+SUCCESS=False
+
+
 def read_table(filename='foo.txt',format=''):
     '''
     Read a file using astropy.io.ascii and
@@ -167,55 +171,87 @@ def log2table(logfile='commits.txt',masterfile='retro_master.txt'):
     
     print(len(commit),len(author),len(date),len(info))
     
-    xnum=np.linspace(1,len(commit),len(commit),dtype=int)
+    xnum=np.linspace(0,len(commit)-1,len(commit),dtype=int)
     
     result=Table([xnum,commit,author,date,info], names=['Number','Commit','Authur','Date','Info'])
 
     result['Compiled']='No'
     result['Changed']='Unknown'
-    result['Run']='No'
+    result['Ran']='No'
     
     result.write(masterfile,format='ascii.fixed_width_two_line',overwrite=True)
+
+    return SUCCESS
         
+PYTHON_SOURCE_DIRECTORY=''
+
+def get_python_source_directory():
+    global PYTHON_SOURCE_DIRECTORY
+
+    result=subprocess.run(['which','py'],capture_output=True,text=True)
+    location=str(result.stdout)
+    x=location.strip()
+    xx=x.replace('bin/py','source/')
+
+    PYTHON_SOURCE_DIRECTORY=xx
+    print('Set Python source directory to :',PYTHON_SOURCE_DIRECTORY)
+
+    return  PYTHON_SOURCE_DIRECTORY 
 
 
-def compile_one(commit='a77ec180c017244cb56f41b50178daf81541748a',number=35,print_output=True):
+def compile_one(commit='a77ec180c017244cb56f41b50178daf81541748a',number=35,print_output=False):
     '''
     Compile a single commit of Python and move the executable to a local directory.
     The name of the executable is determined  by the number
     '''
-    cwd=os.getcwd()
-    os.chdir('/Users/long/Python/source/')
+
+    # cwd=os.getcwd()
+
+    if PYTHON_SOURCE_DIRECTORY=='':
+        get_python_source_directory()
+    os.chdir(PYTHON_SOURCE_DIRECTORY)
+
     result=subprocess.run(['git', 'checkout', commit],capture_output=True,text=True)
+    if result.returncode:
+        print ('Error: Could not checkout new branch, commit all changes before rerunning')
+        print("stderr:", result.stderr)
+        return FAILURE
     if print_output:
         print("stdout:", result.stdout)
-        print("stderr:", result.stderr)
+
     result=subprocess.run(['make', 'clean'],capture_output=True,text=True)
-    if print_output:
-        print("stdout:", result.stdout)
-        print("stderr:", result.stderr)
+
     version='VERSION=_x%03d' % number
     
     result=subprocess.run(['make', version, 'python'],capture_output=True,text=True)
+    if result.returncode:
+        print('Error: Could not compile commit %s',commit)
+        print("stderr:", result.stderr)
+        return FAILURE
+
     if print_output:
         print("stdout:", result.stdout)
         print("stderr:", result.stderr)
     
 
-    if os.path.isfile(cwd+'/py_x%03d' % number):
-        os.remove(cwd+'/py_x%03d' % number)
+    if os.path.isfile(home+'/py_x%03d' % number):
+        os.remove(home+'/py_x%03d' % number)
+    # if os.path.isfile(cwd+'/py_x%03d' % number):
+    #     os.remove(cwd+'/py_x%03d' % number)
     
-    success=True
+    success=SUCCESS
     try:
-        shutil.move('../bin/py_x%03d' % number,cwd)
+        shutil.move('../bin/py_x%03d' % number,home)
+        # shutil.move('../bin/py_x%03d' % number,cwd)
     except:
         print('Could not find ../bin/py_x%03d for commit %s' %(number,commit))
-        success=False
+        success=FAILURE
 
     # return the repository to a known state
     result=subprocess.run(['git', 'checkout', 'dev'],capture_output=True,text=True)
 
-    os.chdir(cwd)
+    # os.chdir(cwd)
+    os.chdir(home)
     return success
 
 
@@ -225,14 +261,18 @@ def check_inputs(command='py',parameter_file='cv.pf',print_output=True):
     Check that a parameter file has the appropriate parameters to run with 
     a version of python
     '''
-    result=subprocess.run([command, '-i', parameter_file],capture_output=True,text=True,timeout=5)
+    try:
+        result=subprocess.run([command, '-i', parameter_file],capture_output=True,text=True,timeout=5)
+    except:
+        print('Executable %s appears missing' % command)
+        return FAILURE
     if print_output:
         print("stdout:", result.stdout)
         print("stderr:", result.stderr)
     if result.stdout.count('Exiting'):
         print('Parameter File problem with %s %s' % (command,parameter_file))
-        return False
-    return True
+        return FAILURE
+    return SUCCESS
 
 
 
@@ -244,10 +284,10 @@ def check_completion(parameter_file='cv.pf'):
     xsig=root+'.sig'
     f=open(xsig)
     lines=f.readlines()
-    print(lines[-1])
+    print(lines[-1].strip())
     if lines[-1].count('COMPLETE'):
-        return True
-    return('False')
+        return SUCCESS
+    return  FAILURE
 
 def run_one(command='py',parameter_file='cv.pf',np=8, print_output=False,timeout=3000):
     '''
@@ -257,24 +297,80 @@ def run_one(command='py',parameter_file='cv.pf',np=8, print_output=False,timeout
     python subprocess call.
     '''
     
-    if check_inputs(command,parameter_file,print_output)==False:
-        return False
+    result=subprocess.run(['Setup_Py_Dir'],capture_output=True,text=True)
+
+    if check_inputs(command,parameter_file,print_output)==FAILURE:
+        print('Inputs failed')
+        return FAILURE
     
     try:
-        result=subprocess.run(['Setup_Py_Dir'],capture_output=True,text=True)
         result=subprocess.run(['mpirun', '-np',str(np),command, parameter_file],capture_output=True,text=True,timeout=timeout)
     except subprocess.TimeoutExpired:
-        return False
+        print('Failed for timeout')
+        return FAILURE
+
+    if result.returncode:
+        print ('Error: could not run command ',command)
+        print("stderr:", result.stderr)
+        return FAILURE
+
+
     if print_output:
         print("stdout:", result.stdout)
         print("stderr:", result.stderr)
     if result.stdout.count('Exiting'):
         print('Parameter File problem with %s %s' % (command,parameter_file))
-        return False
+        return FAILURE
     return check_completion(parameter_file)
 
 
-def compile_many(masterfile='retro_master.txt',increment=10):
+def check4differences(masterfile='retro_master.txt',print_output=True):
+    '''
+    Check for changes between compiled versions
+    '''
+    os.chdir(home)
+    xsum=read_table(masterfile)
+
+    n_compiled=0
+    n_changed=0
+    n_unchanged=0
+    n_error=0
+
+
+    i=0
+    old=-1
+    while i<len(xsum):
+        if xsum['Compiled'][i]=='Yes':
+            n_compiled+=1
+            if old== -1:
+                xsum['Changed'][i]='Yes'
+                n_changed+=1
+            else:
+                try:
+                    if os.path.getsize('py_x%03d' % xsum['Number'][old])==os.path.getsize('py_x%03d' % xsum['Number'][i]):
+                        xsum['Changed'][i]='No'
+                        if print_output:
+                            print('No Change for py_x%03d vs py_x%03d' % (xsum['Number'][i],xsum['Number'][old]) )
+                        n_unchanged+=1
+                    else:
+                        if print_output:
+                            print('   Change for py_x%03d vs py_x%03d' % (xsum['Number'][i],xsum['Number'][old]) )
+                        xsum['Changed'][i]='Yes'
+                        n_changed+=1
+                except:
+                    xsum['Changed'][i]='Unknown'
+                    n_error+=1
+            old=i
+        i+=1
+    xsum.write(masterfile,format='ascii.fixed_width_two_line',overwrite=True)
+
+    print('Of %d compiled, %d changed %d were not changed and there were %d problems' % 
+            (n_compiled, n_changed, n_unchanged, n_error))
+    return
+            
+
+
+def compile_many(masterfile='retro_master.txt',increment=10, nmin=0,nmax=0,rerun=False):
     '''
     This routine compiles a number of versions of python and checks
     to see if the code has changed between the time it has changed.
@@ -284,30 +380,42 @@ def compile_many(masterfile='retro_master.txt',increment=10):
     xsum=read_table(masterfile)
     i=0
     old=-1
-    while i<len(xsum):
-        print(xsum['Commit'][i],xsum['Number'][i])
-        success=compile_one(xsum['Commit'][i],xsum['Number'][i],print_output=False)
-        if success:
-            xsum['Compiled'][i]='Yes'
-            new=i
-            if old== -1:
-                xsum['Changed'][i]='Yes'
-            else:
-                if os.path.getsize('py_x%03d' % xsum['Number'][old])==os.path.getsize('py_x%03d' % xsum['Number'][i]):
-                    xsum['Changed'][i]='No'
-                    print('No Change for py_x%03d' % (xsum['Number'][i]) )
-                else:
-                    xsum['Changed'][i]='Yes'
-            old=i
+    
+    if  nmin == nmax:
+        imin=0
+        imax=len(xsum)
+    else:
+        if nmax<len(xsum):
+            imax=nmax
         else:
-            xsum['Compiled'][i]='Failed'
-            xsum['Changed'][i]='Failed'
+            imax=len(xsum)
+        imin=0
+        if nmin>0 and nmin<nmax:
+            imin=nmin
+
+    i=imin
+    while i<=imax:
+        # print(xsum['Commit'][i],xsum['Number'][i])
+        if rerun == False and  xsum['Compiled'][i] == 'Yes':
+            pass
+        else:
+            success=compile_one(xsum['Commit'][i],xsum['Number'][i],print_output=False)
+            print('Tried to compile commit %s or no %d, which returned %d  where SUCCESS== %d' % (xsum['Commit'][i],xsum['Number'][i],success,SUCCESS))
+            if success == SUCCESS:
+                # print('Yes it was successful')
+                xsum['Compiled'][i]='Yes'
+            else:
+                xsum['Compiled'][i]='Failed'
+                xsum['Changed'][i]='Failed'
+        # print(xsum[i])
         xsum.write(masterfile,format='ascii.fixed_width_two_line',overwrite=True)
         i+=increment
+
+    check4differences(masterfile,print_output=False)
     return 
 
 
-def run_many(master='retro_master.txt',parameter_file='cv.pf',np=8,rerun=False):
+def run_many(master='retro_master.txt',parameter_file='cv.pf',np=8,nmin=0,nmax=0,rerun=False,run_changed_only=False):
     '''
     Run many versions of python on a single parameter file.
     
@@ -316,31 +424,80 @@ def run_many(master='retro_master.txt',parameter_file='cv.pf',np=8,rerun=False):
     In particular if any special files are called then the path to the
     files must be defined correctly, and any set of model files must
     be defined so that they will be found from the subdirectory as well.
+
+    nmin and nmax indicate the range of models to run
+
+    rerun indicates that you want to rerun models that have been rerun previously. (If
+    you want to run a differenct parameter file, without resetting the master file up,
+    you should use this option.)
+
+    if changed_only is True, then you will only run models whose binary file length
+    has changed from the previous commit.  Changes of large scope generally change the
+    size of the binary file, but for small changes the file lennght can stat the same,
+    one might want to use this option as a way to coarsely isolate a problem in one of the
+    commits, but one should be aware that small canges to not necessarily change the
+    binary file length.
+
+    Note that if one is sure there has been a large change between commits and the
+    file lenght does not change, be sure to chack that the commit number is what 
+    one expects between the two runs.
+
     '''
+
     start_dir=os.getcwd()
     x=read_table(master)
     # Check that the file contains the column Ran, and if not add it
-    check=False
-    for one in x.colnames:
-        if one=='Ran':
-            check=True
-    if rerun or check==False:
-        x['Ran']='           No'
     
-    for one in x:
-        if one['Changed']=='Yes' and one['Ran']!='Yes':
+    if  nmin == nmax:
+        imin=0
+        imax=len(x)
+    else:
+        if nmax<len(x):
+            imax=nmax
+        else:
+            imax=len(x)
+        imin=0
+        if nmin>0 and nmin<nmax:
+            imin=nmin
+
+    print('Running for versions of Python from %d to %d' % (nmin,nmax))
+    if run_changed_only:
+        print('Only running models for which the binary file length changes between models')
+        print('Warning: Not all code changes result in changes to the binary file length')
+    if rerun:
+        print('Rerunning all models, not just those that had not been run previously')
+
+    i=imin
+    while i<imax:
+        one=x[i]
+
+  
+
+        if rerun:
+            one['Ran']='No'
+
+        if one['Compiled']!='Yes':
+            pass
+        elif one['Ran']=='Yes':
+            pass
+        elif one['Changed']=='Yes' or run_changed_only == False:
             xcommand='py_x%03d' % one['Number']
+            print('Starting %s' % xcommand)
             if os.path.isdir('r_'+xcommand)==False:
                 os.mkdir('r_'+xcommand)
             shutil.copy(parameter_file,'r_'+xcommand)
             os.chdir('r_'+xcommand)
+
             success=run_one(command='../'+xcommand,parameter_file=parameter_file,np=np, print_output=False,timeout=8000)
-            if success==True:
+            if success==SUCCESS:
                 one['Ran']='Yes'
+                print('%s completed successfully' % (xcommand))
             else:
                 one['Ran']='Failed'
+                print('%s failed to complete' % (xcommand))
             os.chdir(start_dir)
             x.write(master,format='ascii.fixed_width_two_line',overwrite=True)
+        i+=1
     return 
 
 
@@ -418,20 +575,15 @@ def plot_two(dir1='r_py_x001',dir2='r_py_x026',file='agn.spec',colname='A01P0.50
 
 
 
-
-
-# Next lines permit one to run the routine from the command line
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv)>1:
-        # doit(int(sys.argv[1]))
-        doit(sys.argv[1])
-    else:
-        print ('usage: xsmooth filename')
-
 def plot_many(masterfile='retro_master.txt',file='agn.spec',colname='A01P0.50',smooth=11,xlim=[0,0],ylim=[0,0]):
     '''
-    Plot spectra from a set of models all of which should h
+    Plot spectra from a set of models all of which should in various sub-dirctories.
+
+    Here:
+
+    file is the name of the file one of whose columns is to be plotted
+    colname is the column to plot
+    smooth 
     '''
     x=ascii.read(masterfile)
     xx=x[x['Ran']=='Yes']
