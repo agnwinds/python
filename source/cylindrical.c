e46b0fc04f6c72e6b848bb5b7da4d01878492c79
Knox S. Long <long@stsci.edu>
2022-01-13 09:45:51 -0600

    Change to cylindrical to include expansion of grid into partial cells

--- source/cylindrical.c
+++ source/cylindrical.c
@@ -651,8 +651,8 @@ cylind_extend_density (ndom, w)
     for (j = 0; j < mdim - 1; j++)
     {
       wind_ij_to_n (ndom, i, j, &n);
-      if (w[n].vol == 0)
-//DEBUG      if (w[n].vol == 0 || (modes.exclude_partial_cells && w[n].inwind == W_PART_INWIND))
+//DEBUG    if (w[n].vol == 0)
+      if (w[n].vol == 0 || (modes.exclude_partial_cells && w[n].inwind == W_PART_INWIND))
 
       {                         /*Then this grid point is not in the wind  */
 
