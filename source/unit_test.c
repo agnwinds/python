d5e67b760bec851a76276f872311408eb522f36e
Knox S. Long <long@stsci.edu>
2022-05-17 16:39:37 -0500

    Non working attempt to create a parallelized version of wind_luminosity (in unit_test) It failes because of Bcast issues

--- source/unit_test.c
+++ source/unit_test.c
@@ -22,19 +22,15 @@
 #include "atomic.h"
 #include "python.h"
 
+char inroot[LINELENGTH];
 
+#define COMPTON_TEST 0
 
 int
 main (argc, argv)
      int argc;
      char *argv[];
 {
-  int n;
-  double velocity[3];
-  double t;
-  double v;
-  double speed = 0;
-  double speed2 = 0;
 
 //  FILE *fptr, *fopen ();
 
@@ -58,8 +54,16 @@ main (argc, argv)
 
   init_rand (1084515760 + (13 * rank_global));
 
+  printf ("Beginning unit test\n");
 
 
+#if COMPTON_TEST == 1
+  int n;
+  double velocity[3];
+  double t;
+  double v;
+  double speed = 0;
+  double speed2 = 0;
   t = 1.0e4;
 
   for (n = 0; n < 1000000; n++)
@@ -72,12 +76,197 @@ main (argc, argv)
 
   }
 
-
   printf (" Average speed %e  average v**2  %e\n", speed / n, speed2 / n);
 
   printf (" The expected average is %e\n", 2. / sqrt (3.141592) * sqrt (2. * BOLTZMANN * t / MELEC));
+#endif
 
 
-  exit (0);
+/* Create a way to do luminosities in parallel */
 
+  char infile[LINELENGTH];
+  double lum_one;
+  int zparse ();
+
+  double f1 = 1e12;
+  double f2 = 1e18;
+
+
+  zparse (argc, argv);
+  sprintf (infile, "%.150s.wind_save", inroot);
+  printf ("Reading %s \n", infile);
+  wind_read (infile);
+
+
+
+
+
+
+  lum_one = wind_luminosity (f1, f2, MODE_CMF_TIME);
+
+  if (my_rank == 0)
+  {
+    printf ("Old version: %e %e %e %e\n", lum_one, geo.lum_lines, geo.lum_rr, geo.lum_ff);
+  }
+
+
+
+  double par_wind_luminosity ();
+
+  lum_one = par_wind_luminosity (f1, f2, MODE_CMF_TIME);
+
+  Log_parallel ("new version: %d %e %e %e %e\n", my_rank, lum_one, geo.lum_lines, geo.lum_rr, geo.lum_ff);
+
+  printf ("Finished unit test\n");
+
+
+  return (0);
+
+}
+
+
+int
+zparse (argc, argv)
+     int argc;
+     char *argv[];
+{
+  char dummy[LINELENGTH];
+
+  if (argc != 2)
+  {
+    printf ("usage: unit_test root\n");
+    exit (1);
+  }
+
+
+  strcpy (dummy, argv[1]);
+  get_root (inroot, dummy);
+
+
+
+  return (0);
+
+}
+
+
+
+double
+par_wind_luminosity (f1, f2, mode)
+     double f1, f2;
+     int mode;
+{
+  double lum, lum_lines, lum_rr, lum_ff, factor;
+  double x;
+  int nplasma;
+  int ndo, my_nmin, my_nmax, n;
+
+
+  lum = lum_lines = lum_rr = lum_ff = factor = 0.0;
+
+
+
+#ifdef MPI_ON
+  ndo = get_parallel_nrange (rank_global, NPLASMA, np_mpi_global, &my_nmin, &my_nmax);
+  // Log parallel gets each thread as does printf
+  Log_parallel ("xxxwind_luminosity: thread %d lum %d to %d %d \n", rank_global, my_nmin, my_nmax, ndo);
+  Log ("wind_luminosity: thread %d lum %d to %d %d \n", rank_global, my_nmin, my_nmax, ndo);
+#else
+  my_nmin = 0;
+  my_nmax = NPLASMA;
+  ndo = NPLASMA;
+#endif
+
+
+  Log_parallel ("Hello world %d\n", rank_global);
+
+
+  for (nplasma = my_nmin; nplasma < my_nmax; nplasma++)
+  {
+
+    if (wmain[plasmamain[nplasma].nwind].inwind < 0)
+    {
+      Error ("wind_luminosty: Trying to calculate luminosity for a wind cell %d that has plasma cell %d but is not in the wind\n",
+             plasmamain[nplasma].nwind, nplasma);
+    }
+
+
+    if (mode == MODE_OBSERVER_FRAME_TIME)
+      factor = 1.0 / plasmamain[nplasma].xgamma;        /* this is dt_cmf */
+    else if (mode == MODE_CMF_TIME)
+      factor = 1.0;
+
+    lum += x = total_emission (&plasmamain[nplasma], f1, f2) * factor;
+    lum_lines += plasmamain[nplasma].lum_lines * factor;
+    lum_rr += plasmamain[nplasma].lum_rr * factor;
+    lum_ff += plasmamain[nplasma].lum_ff * factor;
+  }
+
+  /* So at this point I have calculated the lumiosities in one of the threads for all of the plasma cells,
+     but i now need to get this informationm to all of the threads.   Presumably the values are undetermined
+     in the cells I have not calculated
+   */
+
+#ifdef MPI_ON
+  int size_of_commbuffer, n_mpi, n_mpi2, num_comm;
+  int position, i;
+  char *commbuffer;
+
+  size_of_commbuffer = 8 * (floor (NPLASMA / np_mpi_global) + 1);
+  size_of_commbuffer = 24 * (floor (NPLASMA / np_mpi_global) + 10);
+  commbuffer = (char *) malloc (size_of_commbuffer * sizeof (char));
+
+  Log ("commbuffer size %d\n", size_of_commbuffer);
+
+
+  MPI_Barrier (MPI_COMM_WORLD);
+
+  for (n_mpi = 0; n_mpi < np_mpi_global; n_mpi++)
+  {
+    Log ("MPI task %d is working on cells %d to max %d (total size %d).\n", rank_global, my_nmin, my_nmax, NPLASMA);
+
+    position = 0;
+
+    if (rank_global == n_mpi)
+    {
+      MPI_Pack (&ndo, 1, MPI_INT, commbuffer, size_of_commbuffer, &position, MPI_COMM_WORLD);
+      for (n = my_nmin; n < my_nmax; n++)
+      {
+        MPI_Pack (&n, 1, MPI_INT, commbuffer, size_of_commbuffer, &position, MPI_COMM_WORLD);
+        MPI_Pack (&plasmamain[n].lum_lines, 1, MPI_DOUBLE, commbuffer, size_of_commbuffer, &position, MPI_COMM_WORLD);
+
+        Log ("Position %d %d\n", n, position);
+      }
+    }
+    Log ("MPI task %d broadcasting plasma update information.\n", rank_global);
+  }
+
+  MPI_Barrier (MPI_COMM_WORLD);
+  MPI_Bcast (commbuffer, size_of_commbuffer, MPI_PACKED, n_mpi, MPI_COMM_WORLD);
+  MPI_Barrier (MPI_COMM_WORLD);
+  Log ("Luminosity: MPI task %d survived broadcasting matom_matrix update information.\n", rank_global);
+
+  position = 0;
+
+  if (rank_global != n_mpi)
+  {
+    MPI_Unpack (commbuffer, size_of_commbuffer, &position, &num_comm, 1, MPI_INT, MPI_COMM_WORLD);
+    for (n_mpi2 = 0; n_mpi2 < num_comm; n_mpi2++)
+    {
+      MPI_Unpack (commbuffer, size_of_commbuffer, &position, &n, 1, MPI_INT, MPI_COMM_WORLD);
+      MPI_Unpack (commbuffer, size_of_commbuffer, &position, &plasmamain[n].lum_lines, 1, MPI_DOUBLE, MPI_COMM_WORLD);
+
+    }
+  }
+  free (commbuffer);
+
+#endif
+
+  if (mode == MODE_CMF_TIME)
+  {
+    geo.lum_lines = lum_lines;
+    geo.lum_rr = lum_rr;
+    geo.lum_ff = lum_ff;
+  }
+
+  return (lum);
 }
