b4b8d1057d8afce926aea443e738dfd83ab6adbd
Knox S. Long <long@stsci.edu>
2022-05-30 16:30:10 -0500

    Updated frame.c to use lorentz

--- source/frame.c
+++ source/frame.c
@@ -206,59 +206,6 @@ local_to_observer_frame (p_in, p_out)
  *
  **********************************************************/
 
-// int
-// observer_to_local_frame_disk (p_in, p_out)
-//      PhotPtr p_in, p_out;
-// {
-//   double f_out, f_in;
-//   double x;
-//   double v[3], vel;
-//   double gamma;
-//   int i, ierr;
-
-
-
-
-//   ierr = check_frame (p_in, F_OBSERVER, "Observer_to_local_frame_disk");
-
-//   /* Initialize the output photon */
-//   stuff_phot (p_in, p_out);
-//   p_out->frame = F_LOCAL;
-
-  /* Calculate the local velocity of the disk at this position */
-//   vdisk (p_in->x, v);
-
-//   vel = dot (p_in->lmn, v);
-
-
-//   f_in = p_in->freq;
-
-//   if (rel_mode == REL_MODE_LINEAR)
-//   {
-//     f_out = p_out->freq = f_in * (1. - vel / VLIGHT);
-//     return (ierr);
-//   }
-
-
-
-  // beta=length(v)/VLIGHT;
-
-//   gamma = 1. / (sqrt (1 - (dot (v, v) / (VLIGHT * VLIGHT))));
-
-//   f_out = p_out->freq = f_in * gamma * (1. - vel / VLIGHT);
-
-//   x = gamma / VLIGHT * (1.0 - (gamma * vel / ((gamma + 1) * VLIGHT)));
-
-//   for (i = 0; i < 3; i++)
-//   {
-//     p_out->lmn[i] = f_in / f_out * (p_in->lmn[i] - x * v[i]);
-//   }
-
-//   p_out->w *= (f_out / f_in);
-
-
-//   return (ierr);
-// }
 
 
 
@@ -322,66 +269,12 @@ observer_to_local_frame_disk (p_in, p_out)
  *
  **********************************************************/
 
-// int
-// local_to_observer_frame_disk (p_in, p_out)
-//      PhotPtr p_in, p_out;
-// {
-//   double f_out, f_in;
-//   double x;
-//   double v[3], vel;
-//   double gamma;
-//   int i;
-//   int ierr;
-
-//   ierr = check_frame (p_in, F_LOCAL, "local_to_observer_frame_disk");
-
-//   /* Initialize the output photon */
-//   stuff_phot (p_in, p_out);
-//   p_out->frame = F_OBSERVER;
-
-
-//   /* Calculate the local velocity of the disk at this position */
-//   vdisk (p_in->x, v);
-//   vel = dot (p_in->lmn, v);
-
-//   f_in = p_in->freq;
-
-
-
-  /* Note the use of division here; it is intended, so that this produces the exact
-     opposite of the transformation in the other direction */
-
-//   if (rel_mode == REL_MODE_LINEAR)
-//   {
-//     f_out = p_out->freq = f_in / (1. - vel / VLIGHT);
-//     return (ierr);
-//   }
-
-//   gamma = 1. / (sqrt (1 - (dot (v, v) / (VLIGHT * VLIGHT))));
-//   f_out = p_out->freq = f_in * gamma * (1. + vel / VLIGHT);
-
-/* Need to worry about sign changes, etc. here */
-//   x = gamma / VLIGHT * (1.0 + (gamma * vel / ((gamma + 1) * VLIGHT)));
-
-//   for (i = 0; i < 3; i++)
-//   {
-//     p_out->lmn[i] = f_in / f_out * (p_in->lmn[i] + x * v[i]);
-//   }
-
-//   p_out->w *= (f_out / f_in);
-
-
-
-//   return (ierr);
-// }
 
 
 int
 local_to_observer_frame_disk (p_in, p_out)
      PhotPtr p_in, p_out;
 {
-//  WindPtr one;
-//  int ndom;
   double v[3];
   int ierr;
 
@@ -432,7 +325,6 @@ observer_to_local_frame_ds (p_obs, ds_obs)
 {
   WindPtr one;
   int ndom;
-//OLD  double v[3], vel;
   double v[3];
   double gamma;
   double ds_cmf;
@@ -446,7 +338,6 @@ observer_to_local_frame_ds (p_obs, ds_obs)
   one = &wmain[p_obs->grid];
   ndom = one->ndom;
   vwind_xyz (ndom, p_obs, v);
-//OLD  vel = dot (p_obs->lmn, v);
 
   gamma = 1. / sqrt (1 - (dot (v, v) / (VLIGHT * VLIGHT)));
 
@@ -492,7 +383,6 @@ local_to_observer_frame_ds (p_obs, ds_cmf)
 {
   WindPtr one;
   int ndom;
-//OLD  double v[3], vel;
   double v[3];
   double gamma;
   double ds_obs;
@@ -506,7 +396,6 @@ local_to_observer_frame_ds (p_obs, ds_cmf)
   one = &wmain[p_obs->grid];
   ndom = one->ndom;
   vwind_xyz (ndom, p_obs, v);
-//OLD  vel = dot (p_obs->lmn, v);
 
   gamma = 1. / sqrt (1 - (dot (v, v) / (VLIGHT * VLIGHT)));
 
