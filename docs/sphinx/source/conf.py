e6e836f6bf7023f46fbaf7aa960c613dc2af379d
jhmatthews <matthews@ast.cam.ac.uk>
2022-05-31 12:53:09 +0100

    changed path to abspath in conf.py and reorg python scripts docs

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -45,8 +45,9 @@ extensions = [
 
 import os
 import sys
-
-sys.path.insert(0, '../../../../py_progs/')
+# sys.path.insert(0, os.path.abspath('../../../'))
+sys.path.insert(0, os.path.abspath('../../../py_progs/'))
+sys.path.insert(0, os.path.abspath('../../../py_progs/py4py'))
 
 # == Options for the varous plugins =======================================
 # -- Whether or not to include ToDo notes ---------------------------------
