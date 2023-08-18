0549c904048c2205de1d49b7833a6b205f29a1fd
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2023-08-18 14:22:33 +0100

    changes to test on endjinn

--- source/matrix.h
+++ source/matrix.h
@@ -12,12 +12,16 @@
 #define MATRIX_H
 #ifdef CUDA_ON
 
+/* initialise and destroy cuda/cusolver */
 int cuda_init (void);
 int cuda_finish (void);
-const char *get_gpu_solve_matrix_error_string (cusolverStatus_t status);
 
+/* gpu matrix routines */
 int gpu_solve_matrix (double *a_matrix, double *b_vector, int size, double *x_vector);
 int gpu_invert_matrix (double *matrix, double *inverse, int num_rows);
+
+/* error string */
+const char *get_gpu_solve_matrix_error_string (int error);
 
 #endif
 #endif
