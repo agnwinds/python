f7816b37e3cfaa4dd783da94afc29d0dc06e8b41
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-01-15 13:14:00 +0000

    simplify spectrum communication using all-to-all reduction

--- source/communicate_spectra.c
+++ source/communicate_spectra.c
@@ -19,23 +19,30 @@
 
 /**********************************************************/
 /**
- * @brief sum up the synthetic and cell spectra between threads.
+ * @brief Normalize the synthetic spectra across ranks.
  *
  * @details
- * sum up the synthetic spectra between threads. Does an
- * MPI_Reduce then an MPI_Bcast for each element of the
- * linear and log spectra arrays (xxspec)
+ *
+ * Uses an all-to-all reduction to sum up and normalize the synthetic spectra
+ * between MPI ranks using an MPI_Allreduce. Works on both the linear and the
+ * log spectra.
  *
  **********************************************************/
 
 int
-gather_extracted_spectrum (void)
+nomralize_spectra_across_ranks (void)
 {
 #ifdef MPI_ON
-  double *redhelper, *redhelper2;
-  int mpi_i, mpi_j;
-  int size_of_commbuffer, nspec;
+  int i;
+  int j;
+  int nspec;
+  int size_of_commbuffer;;
+  double *spectrum_buffer;
 
+  xsignal (files.root, "%-20s Begin spectrum reduction in ionization cycles\n", "NOK");
+
+  /* In ionization cycles, we don't need to normalize/reduce an additional
+   * geo.nangles, as these are for the observer angles in spectrum cycles */
   if (geo.ioniz_or_extract == CYCLE_EXTRACT)
   {
     nspec = MSPEC + geo.nangles;
@@ -45,39 +52,35 @@ gather_extracted_spectrum (void)
     nspec = MSPEC;
   }
 
-  size_of_commbuffer = 4 * nspec * NWAVE_MAX;   //we need space for all 4 separate spectra we are normalizing
+  size_of_commbuffer = 4 * nspec * NWAVE_MAX;   // We need space for all 4 separate spectra we are normalizing
+  spectrum_buffer = calloc (sizeof (double), size_of_commbuffer);
 
-  redhelper = calloc (sizeof (double), size_of_commbuffer);
-  redhelper2 = calloc (sizeof (double), size_of_commbuffer);
-
-  for (mpi_i = 0; mpi_i < NWAVE_MAX; mpi_i++)
+  for (i = 0; i < NWAVE_MAX; i++)
   {
-    for (mpi_j = 0; mpi_j < nspec; mpi_j++)
+    for (j = 0; j < nspec; j++)
     {
-      redhelper[mpi_i * nspec + mpi_j] = xxspec[mpi_j].f[mpi_i] / np_mpi_global;
-      redhelper[mpi_i * nspec + mpi_j + (NWAVE_MAX * nspec)] = xxspec[mpi_j].lf[mpi_i] / np_mpi_global;
-      redhelper[mpi_i * nspec + mpi_j + (2 * NWAVE_MAX * nspec)] = xxspec[mpi_j].f_wind[mpi_i] / np_mpi_global;
-      redhelper[mpi_i * nspec + mpi_j + (3 * NWAVE_MAX * nspec)] = xxspec[mpi_j].lf_wind[mpi_i] / np_mpi_global;
+      spectrum_buffer[i * nspec + j] = xxspec[j].f[i] / np_mpi_global;
+      spectrum_buffer[i * nspec + j + (NWAVE_MAX * nspec)] = xxspec[j].lf[i] / np_mpi_global;
+      spectrum_buffer[i * nspec + j + (2 * NWAVE_MAX * nspec)] = xxspec[j].f_wind[i] / np_mpi_global;
+      spectrum_buffer[i * nspec + j + (3 * NWAVE_MAX * nspec)] = xxspec[j].lf_wind[i] / np_mpi_global;
     }
   }
 
-  MPI_Reduce (redhelper, redhelper2, size_of_commbuffer, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  MPI_Bcast (redhelper2, size_of_commbuffer, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Allreduce (MPI_IN_PLACE, spectrum_buffer, size_of_commbuffer, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-  for (mpi_i = 0; mpi_i < NWAVE_MAX; mpi_i++)
+  for (i = 0; i < NWAVE_MAX; i++)
   {
-    for (mpi_j = 0; mpi_j < nspec; mpi_j++)
+    for (j = 0; j < nspec; j++)
     {
-      xxspec[mpi_j].f[mpi_i] = redhelper2[mpi_i * nspec + mpi_j];
-      xxspec[mpi_j].lf[mpi_i] = redhelper2[mpi_i * nspec + mpi_j + (NWAVE_MAX * nspec)];
-      xxspec[mpi_j].f_wind[mpi_i] = redhelper2[mpi_i * nspec + mpi_j + (2 * NWAVE_MAX * nspec)];
-      xxspec[mpi_j].lf_wind[mpi_i] = redhelper2[mpi_i * nspec + mpi_j + (3 * NWAVE_MAX * nspec)];
+      xxspec[j].f[i] = spectrum_buffer[i * nspec + j];
+      xxspec[j].lf[i] = spectrum_buffer[i * nspec + j + (NWAVE_MAX * nspec)];
+      xxspec[j].f_wind[i] = spectrum_buffer[i * nspec + j + (2 * NWAVE_MAX * nspec)];
+      xxspec[j].lf_wind[i] = spectrum_buffer[i * nspec + j + (3 * NWAVE_MAX * nspec)];
     }
   }
 
-  free (redhelper);
-  free (redhelper2);
+  free (spectrum_buffer);
+  xsignal (files.root, "%-20s Finished spectrum reduction in ionization cycles\n", "OK");
 #endif
-
   return (0);
 }
