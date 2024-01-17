fd497d6b542c05b81dc741d194e6f11aadecaaee
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-01-17 12:42:52 +0000

    moving loading model grid into its own source for testing

--- source/tests/tests/test_define_wind.c
+++ source/tests/tests/test_define_wind.c
@@ -21,290 +21,14 @@
 #include "../../atomic.h"
 #include "../../python.h"
 #include "../assert.h"
+#include "../unit_test.h"
 
 char *PYTHON_ENV;
 char TEST_CWD[LINELENGTH];
 char ATOMIC_DATA_TARGET[LINELENGTH];
 char ATOMIC_DATA_DEST[LINELENGTH];
 
-#define PATH_SEPARATOR '/'
 #define TEST_DATA_LENGTH 2056
-
-/** *******************************************************************************************************************
- *
- * @brief Free a pointer and set to NULL
- *
- * @param [in]  void** ptr  An address to the pointer to free and set to null
- *
- * @details
- *
- * To use this function, you need to pass the address of a pointer cast as void**, e.g.
- *
- *      free_and_null((void**) &wmain);
- *
- * ****************************************************************************************************************** */
-
-void
-free_and_null (void **ptr)
-{
-  if (ptr != NULL & *ptr != NULL)
-  {
-    free (*ptr);
-    *ptr = NULL;
-  }
-}
-
-/** *******************************************************************************************************************
- *
- * @brief Get the last component in a file path.
- *
- * @details
- *
- * This will return the file name and extension within a file path.
- *
- * ****************************************************************************************************************** */
-
-const char *
-get_last_component (const char *path)
-{
-  const char *last_separator = strrchr (path, PATH_SEPARATOR);
-  return last_separator ? last_separator + 1 : path;
-}
-
-/** *******************************************************************************************************************
- *
- * @brief Find the location of the atomic data, relative to a model.
- *
- * @details
- *
- * This function exists to modify the atomic data in the parameter file to make it an absolute path to where
- * it has been set at suite initialisation.
- *
- * ****************************************************************************************************************** */
-
-int
-set_atomic_data_filename (void)
-{
-  char temp_filepath[LINELENGTH];
-  char atomic_data_filepath[LINELENGTH];
-
-  if (strlen (ATOMIC_DATA_DEST) + strlen (get_last_component (geo.atomic_filename)) >= LINELENGTH)
-  {
-    perror ("New atomic data filepath will be too long for internal buffer");
-    return EXIT_FAILURE;
-  }
-
-  strncpy (temp_filepath, ATOMIC_DATA_DEST, LINELENGTH - 1);
-  temp_filepath[LINELENGTH - 1] = '\0';
-
-  size_t len1 = strlen (temp_filepath);
-  if (len1 > 0 && temp_filepath[len1 - 1] != PATH_SEPARATOR)
-  {
-    strncat (temp_filepath, "/", LINELENGTH - len1 - 1);
-  }
-  snprintf (atomic_data_filepath, LINELENGTH, "%s%s", temp_filepath, get_last_component (geo.atomic_filename));
-
-  if (strlen (atomic_data_filepath) >= LINELENGTH)
-  {
-    perror ("Buffer overflow when creating new atomic data filepath");
-    return EXIT_FAILURE;
-  }
-
-  strcpy (geo.atomic_filename, atomic_data_filepath);
-
-  return EXIT_SUCCESS;
-}
-
-/** *******************************************************************************************************************
- *
- * @brief Clean up after a model has run as a test case.
- *
- * @param [in]  root_name  The root name of the model in $PYTHON/source/tests/test_data/define_wind
- *
- * @details
- *
- * ****************************************************************************************************************** */
-
-int
-cleanup_model (const char *root_name)
-{
-  int n_row;
-  int n_plasma;
-  char parameter_filepath[LINELENGTH];
-
-  PlasmaPtr plasma_cell;
-  MacroPtr macro_cell;
-
-  (void) root_name;
-
-  snprintf (parameter_filepath, LINELENGTH, "%s/source/tests/test_data/define_wind/%s.pf", PYTHON_ENV, files.root);
-  if (cpar (parameter_filepath) != 1)   /* cpar returns 1 when something is "normal" */
-  {
-    return EXIT_FAILURE;
-  }
-
-  /* free domains */
-  free_and_null ((void **) &zdom);
-
-  /* free dynamic grid properties */
-  free_and_null ((void **) &wmain);
-
-  /* NPLASMA + 1 is the dummy plasma cell */
-  for (n_plasma = 0; n_plasma < NPLASMA + 1; ++n_plasma)
-  {
-    plasma_cell = &plasmamain[n_plasma];
-    free (plasma_cell->density);
-    free (plasma_cell->partition);
-    free (plasma_cell->ioniz);
-    free (plasma_cell->recomb);
-    free (plasma_cell->scatters);
-    free (plasma_cell->xscatters);
-    free (plasma_cell->heat_ion);
-    free (plasma_cell->heat_inner_ion);
-    free (plasma_cell->cool_rr_ion);
-    free (plasma_cell->lum_rr_ion);
-    free (plasma_cell->inner_recomb);
-    free (plasma_cell->inner_ioniz);
-    free (plasma_cell->cool_dr_ion);
-    free (plasma_cell->levden);
-    free (plasma_cell->recomb_simple);
-    free (plasma_cell->recomb_simple_upweight);
-    free (plasma_cell->kbf_use);
-  }
-
-  free_and_null ((void **) &plasmamain);
-  free_and_null ((void **) &photstoremain);
-  free_and_null ((void **) &matomphotstoremain);        /* This one doesn't care about if macro atoms are used or not */
-
-  if (nlevels_macro > 0)
-  {
-    for (n_plasma = 0; n_plasma < NPLASMA + 1; n_plasma++)
-    {
-      macro_cell = &macromain[n_plasma];
-      free (macro_cell->jbar);
-      free (macro_cell->jbar_old);
-      free (macro_cell->gamma);
-      free (macro_cell->gamma_old);
-      free (macro_cell->gamma_e);
-      free (macro_cell->gamma_e_old);
-      free (macro_cell->alpha_st);
-      free (macro_cell->alpha_st_old);
-      free (macro_cell->alpha_st_e);
-      free (macro_cell->alpha_st_e_old);
-      free (macro_cell->recomb_sp);
-      free (macro_cell->recomb_sp_e);
-      free (macro_cell->matom_emiss);
-      free (macro_cell->matom_abs);
-      free (macro_cell->cooling_bf);
-      free (macro_cell->cooling_bf_col);
-      free (macro_cell->cooling_bb);
-
-      if (macro_cell->store_matom_matrix == TRUE)
-      {
-        free_and_null ((void **) &macro_cell->matom_matrix);
-      }
-    }
-
-    free_and_null ((void **) &macromain);
-  }
-
-  NDIM2 = 0;
-  NPLASMA = 0;                  /* We probably don't need to do this, but better safe than sorry */
-
-  /* free atomic data elements */
-  free_and_null ((void **) &ele);
-  free_and_null ((void **) &ion);
-  free_and_null ((void **) &xconfig);
-  free_and_null ((void **) &line);
-  free_and_null ((void **) &auger_macro);
-
-
-  return EXIT_SUCCESS;
-}
-
-/** *******************************************************************************************************************
- *
- * @brief Initialise all the necessary parameters to define the wind
- *
- * @param [in]  root_name  The root name of the model in $PYTHON/source/tests/test_data/define_wind
- *
- * @details
- *
- * All the required parameters to define the wind, plasma and macro grid are initialised. There are a number of things
- * which aren't initialised (e.g. binary_basics, get_spec_type, init_observers, etc.) as these are not required to
- * initialise the grids.
- *
- * ****************************************************************************************************************** */
-
-int
-initialise_model_for_define_wind (const char *root_name)
-{
-  int n_dom;
-  char rdchoice_answer[LINELENGTH];
-  char rdchoice_choices[LINELENGTH];
-  char parameter_filepath[LINELENGTH];
-
-  geo.run_type = RUN_TYPE_NEW;
-
-  /* Set up parameter file, that way we can get all the parameters from that
-   * instead of defining them manually */
-  strcpy (files.root, root_name);
-  snprintf (parameter_filepath, LINELENGTH, "%s/source/tests/test_data/define_wind/%s.pf", PYTHON_ENV, files.root);
-  if (opar (parameter_filepath) != 2)   /* opar returns 2 when reading for the parameter file */
-  {
-    fprintf (stderr, "Unable to read from parameter file %s.pf", files.root);
-    return EXIT_FAILURE;
-  }
-
-  zdom = calloc (MAX_DOM, sizeof (domain_dummy));       /* We'll allocate MAX_DOM to follow python */
-  if (zdom == NULL)
-  {
-    fprintf (stderr, "Unable to allocate space for domain structure\n");
-    return EXIT_FAILURE;
-  }
-  init_geo ();
-
-  /* Now when we call the initialisation functions or use rdXXX, the rdchoice_choices
-   * for the parameter will come from the parameter file */
-  rdint ("Wind.number_of_components", &geo.ndomain);
-  if (geo.ndomain > MAX_DOM)
-  {
-    fprintf (stderr, "Using more domains (%d) in model than MAX_DOM (%d)\n", geo.ndomain, MAX_DOM);
-    return EXIT_FAILURE;
-  }
-  strncpy (rdchoice_answer, "star", LINELENGTH);
-  snprintf (rdchoice_choices, LINELENGTH, "%d,%d,%d,%d,%d", SYSTEM_TYPE_STAR, SYSTEM_TYPE_CV, SYSTEM_TYPE_BH, SYSTEM_TYPE_AGN,
-            SYSTEM_TYPE_PREVIOUS);
-  geo.system_type = rdchoice ("System_type(star,cv,bh,agn,previous)", rdchoice_choices, rdchoice_answer);
-
-  /* These routines don't seem to depend on the atomic data or anything which
-   * depends on the atomic data */
-  const double star_lum = get_stellar_params ();
-  get_bl_and_agn_params (star_lum);
-  get_disk_params ();
-
-  /* We have to be a bit creative with the atomic data, to munge the correct
-   * filepath with what's in the parameter file */
-  rdstr ("Atomic_data", geo.atomic_filename);
-  if (set_atomic_data_filename ())
-  {
-    return EXIT_FAILURE;
-  }
-
-  /* We should now be able to initialise everything else which seems to have
-   * some dependence on the ionisation settings or atomic data */
-  init_ionization ();
-  setup_atomic_data (geo.atomic_filename);
-  for (n_dom = 0; n_dom < geo.ndomain; ++n_dom)
-  {
-    get_domain_params (n_dom);
-    get_wind_params (n_dom);
-  }
-  setup_windcone ();
-  DFUDGE = setup_dfudge ();
-
-  return EXIT_SUCCESS;
-}
 
 /** *******************************************************************************************************************
  *
@@ -320,7 +44,7 @@ initialise_model_for_define_wind (const char *root_name)
  *
  * ****************************************************************************************************************** */
 
-void
+static void
 test_sv_agn_macro_wind (void)
 {
   int n;
@@ -331,7 +55,7 @@ test_sv_agn_macro_wind (void)
   WindPtr wind_cell;
   PlasmaPtr plasma_cell;
 
-  const int init_error = initialise_model_for_define_wind ("agn_macro");
+  const int init_error = setup_model_grid ("agn_macro", ATOMIC_DATA_DEST);
   if (init_error)
   {
     CU_FAIL_FATAL ("Unable to initialise AGN Macro model");
@@ -446,7 +170,7 @@ test_sv_agn_macro_wind (void)
  *
  * ****************************************************************************************************************** */
 
-void
+static void
 test_sv_cv_wind (void)
 {
   int n;
@@ -457,7 +181,7 @@ test_sv_cv_wind (void)
   WindPtr wind_cell;
   PlasmaPtr plasma_cell;
 
-  const int init_error = initialise_model_for_define_wind ("cv");
+  const int init_error = setup_model_grid ("cv", ATOMIC_DATA_DEST);
   if (init_error)
   {
     CU_FAIL_FATAL ("Unable to initialise CV model");
@@ -570,7 +294,7 @@ test_sv_cv_wind (void)
  *
  * ****************************************************************************************************************** */
 
-void
+static void
 test_spherical_star_wind (void)
 {
   FILE *fp;
@@ -580,7 +304,7 @@ test_spherical_star_wind (void)
   WindPtr wind_cell;
   PlasmaPtr plasma_cell;
 
-  const int init_error = initialise_model_for_define_wind ("star");
+  const int init_error = setup_model_grid ("star", ATOMIC_DATA_DEST);
   if (init_error)
   {
     cleanup_model ("star");
@@ -697,7 +421,7 @@ test_spherical_star_wind (void)
  *
  * ****************************************************************************************************************** */
 
-int
+static int
 suite_teardown (void)
 {
   if (unlink (ATOMIC_DATA_DEST) != EXIT_SUCCESS)
@@ -726,7 +450,7 @@ suite_teardown (void)
  *
  * ****************************************************************************************************************** */
 
-int
+static int
 suite_init (void)
 {
   struct stat sb;
@@ -776,7 +500,6 @@ suite_init (void)
   /* Now initialise the things Python will need to work, and things which aren't
    * specific to the model being tested such as the domain allocation */
   rel_mode = REL_MODE_FULL;
-
 
   return EXIT_SUCCESS;
 }
---
+++ source/tests/unit_test.h
@@ -0,0 +1,17 @@
+/** ********************************************************************************************************************
+ *
+ *  @file load_model.c
+ *  @author Edward J. Parkinson (e.parkinson@soton.ac.uk)
+ *  @date Jan 2024
+ *
+ *  @brief
+ *
+ * ****************************************************************************************************************** */
+
+#ifndef PYTHON_UNIT_TEST_H
+#define PYTHON_UNIT_TEST_H
+
+int cleanup_model (const char *root_name);
+int setup_model_grid (const char *root_name, const char *atomic_data_location);
+
+#endif
